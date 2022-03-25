f81a6eeed79c2274898069db88f54f28139b6288
Lingo <ElementsPhoenix@163.com>
2022-03-25 21:14:14 +0800

    mod 2022-03-25 21:13:13
    按照按照Julia包DrWatson建议构建实验大框架；

--- src/initialization/fun_setFolders.jl
+++ src/initialization/fun_setFolders.jl
@@ -6,19 +6,34 @@
 #状态/开发
 ##########################################
 
-"函数：设置实验文件夹"
-function setExperimentsFolders(env)
-    if env.filenameTypeOfExperiments=="default"
-        folderpath=projectdir()*"/data/sims/default"
-    elseif env.filenameTypeOfExperiments=="datetime"
-            # NOW
-    elseif env.filenameTypeOfExperiments=="set manually"
-        #TODO
-    else   
-        throw(DomainError(env.filenameTypeOfExperiments, "关键词取值错误！")) 
+"""
+函数：设置实验文件夹
+# Arguments: 
+- env::Dict: 环境变量；
+- isDatetime::Bool = true: 是否加入日期时间；
+"""
+function setExperimentsFolders(env::Dict; isDatetime::Bool = true)
+
+    # 设定日期时间字符串
+    if isDatetime == true
+        str_datetime = "_" * Dates.format(DateTime(now()), "YYYYmmddHHMMSS")
+    else
+        str_datetime = ""
     end
 
-    mkpath(folderpath)
-    
+    ## 设定前缀字符串
+    if env["foldernameTypeOfExperimentsData"] == "default"
+        str_manuallyName = "default"
+    elseif env["foldernameTypeOfExperimentsData"] == "set manually"
+        str_manuallyName = env["foldernamePrefixOfExperimentsData"]
+    else
+        throw(DomainError(env["foldernameTypeOfExperimentsData"], "关键词取值错误！"))
+    end
+
+    foldername = str_manuallyName * str_datetime
+    folderdir = env["rootDirOfExperimentsData"]
+    env["folderpathOfExperimentsData"] = folderdir * foldername
+
+    mkpath(env["folderpathOfExperimentsData"]) # 创建文件夹
 return env
 end # functioin
--- src/process/process_interBank_illiquity_old.jl
+++ src/process/process_interBank_illiquity_old.jl
@@ -5,18 +5,18 @@
 ##########################################
 
 "函数：过程之于流动性短缺银行间挤兑流动传染冲击"
-function process_interBank_illiquity!(BB::BankCommercial, BI::BankInterbank, BB_t1::BankCommercial, BI_t1::BankInterbank, para::Dict, env::EnvironmentVariables)
+function process_interBank_illiquity!(BB::BankCommercial, BI::BankInterbank, BB_t1::BankCommercial, BI_t1::BankInterbank, para::Dict, env::Dict)
 
     ## 过程：流动性短缺银行间挤兑流动传染冲击
-    env.process_name = "流动性短缺银行间挤兑流动传染冲击"
-    println("过程：", env.process_name)
+    env["process_name"] = "流动性短缺银行间挤兑流动传染冲击"
+    println("过程：", env["process_name"])
 
-    while (env.tau <= env.max_num_tau .&& env.is_end_round != true)
+    while (env["tau"] <= env["max_num_tau"] .&& env["is_end_round"] != true)
 
-        env.tau += 1 # 传染回合累加一
-        println("开始回合", env.tau)
+        env["tau"] += 1 # 传染回合累加一
+        println("开始回合", env["tau"])
 
-        ## 初始阶段$t_{0}$，当$\env.tau>1$时：
+        ## 初始阶段$t_{0}$，当$\env["tau"]>1$时：
         println("t0 初始阶段")
 
         b = TypeState{1}(BB.on .|| BB.off) # 临时设置BB示性变量
@@ -40,15 +40,15 @@ function process_interBank_illiquity!(BB::BankCommercial, BI::BankInterbank, BB_
         # update_B_state!(BB, BI; to = "bankrupt", from = "illiquity") # 更新各银行之状态到破产
 
         ## TODO存储数据
-        BB_tau[env.tau] = deepcopy(BB) # 存储该回合传染结果数据
-        BI_tau[env.tau] = deepcopy(BI) # 存储该回合传染结果数据
+        BB_tau[env["tau"]] = deepcopy(BB) # 存储该回合传染结果数据
+        BI_tau[env["tau"]] = deepcopy(BI) # 存储该回合传染结果数据
 
         ## 清零银行内冲击变量
         update_B_Shock!(BB, BI, b, ib; byWay = "clear Shock_B_A and Shock_B_Z") # 清零银行内资产负债冲击
 
         ## 判定本回合是否有银行转移状态，如果无则后续处理然后结束本轮，如果有则继续处理。
         if BB.ilq == BB_t1.ilq
-            env.is_end_round = true
+            env["is_end_round"] = true
             break
         end
 
