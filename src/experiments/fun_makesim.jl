fce70b6a2fb726531e17f18697b5b783e6c35d73
Lingo <ElementsPhoenix@163.com>
2022-03-26 18:03:02 +0800

    mod 2022-03-26 18:02:26
    继续根据Julia工具包Agents构建框架，把之前写的程序放入其中；

--- src/experiments/fun_makesim.jl
+++ src/experiments/fun_makesim.jl
@@ -7,11 +7,11 @@
 
 
 "函数：运行一次仿真"
-function makesim(BB::BankCommercial, BI::BankInterbank, para::Dict, env::EnvironmentVariables)
+function makesim(agent::SystemicRiskAgent, para::Dict, env::EnvironmentVariables)
 
-
-    BB, BI, BB_tau, BI_tau, para, env = model_BI1111(BB, BI, para, env)
-
+    #NOW 写入agent运行步骤在这
+    systemicRiskModel = create_systemicRiskModel(agent, env, para)
+    _, _ = run!(systemicRiskModel, systemicRiskAgent_step!, env[:max_num_tau])
     wsave(datadir(env[:folderpathOfExperimentsData], savename(para, "jld2", connector = "|", equals = "=")), BB) #FIXME
     # return BB, BI, BB_tau, BI_tau, env
 end
