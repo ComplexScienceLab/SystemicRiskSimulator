9e656899bcfc6d7dd1536b839cb03088eecf5764
Lingo <ElementsPhoenix@163.com>
2022-04-04 23:43:15 +0800

    mod 2022-04-04 23:42:27
    分离模型外围框架部分和核心内容部分；

--- test/runtests.jl
+++ test/runtests.jl
@@ -409,29 +409,30 @@ using DrWatson
 #     end
 # )
 
-env = Dict()
-env[:processName] = "a"
-env[:savedProcessName] = "a"
+# using DrWatson
+# @quickactivate "SystemicRisk"
+
+# function add(x, y)
+#     return x + y
+# end
+
+# macro run_process(content)
+#     return esc(quote
+#         if x != y
+#             $(content)
+#         end
+#     end)
+# end
+
+# function fun_runprocess(x, y)
+#     @run_process z = add(x, y)
+#     return z
+# end
 
 
-function add(x, y)
-    return x + y
-end
 
-macro run_process(content)
-    return quote
-        if x != y
-            content
-        end
-    end
-end
+# input1 = 3
+# input2 = 4
+# z=fun_runprocess(input1, input2)
+# println(z)
 
-function fun_runprocess(x, y)
-    @run_process :(z = add(x, y))
-    return z
-end
-
-input1 = 3
-input2 = 4
-fun_runprocess(input1, input2)
-println(z)
