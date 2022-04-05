79ca83f19f3c9abcc9043099cf813ddd5368abfd
Lingo <ElementsPhoenix@163.com>
2022-04-05 23:12:54 +0800

    mod 2022-04-05 23:12:50
    重命名部分文件；

--- src/model/Models.jl
+++ src/model/Models.jl
@@ -19,30 +19,30 @@ include("../model/model_variables/set_stages.jl")
 
 ## 集成阶段文件
 include("../model/stages/stage_list.jl")
-include("../model/stages/stage_exBank_insolvent_shock.jl")
-include("../model/stages/stage_interBank_insolvent_shock.jl")
-include("../model/stages/stage_interBank_insolvent_contagion.jl")
-include("../model/stages/stage_exBank_illiquity_shock.jl")
-include("../model/stages/stage_interBank_illiquity_contagion_shock.jl")
-include("../model/stages/stage_interBank_illiquity_allocate.jl")
-include("../model/stages/stage_interBank_illiquity_repay.jl")
-include("../model/stages/stage_exBank_bankrupt_contagion.jl")
-include("../model/stages/stage_interBank_bankrupt_contagion_shock.jl")
-include("../model/stages/stage_bankrupt_repay_shock.jl")
+include("../model/stages/fun_stage_exBank_insolvent_shock.jl")
+include("../model/stages/fun_stage_interBank_insolvent_shock.jl")
+include("../model/stages/fun_stage_interBank_insolvent_contagion.jl")
+include("../model/stages/fun_stage_exBank_illiquity_shock.jl")
+include("../model/stages/fun_stage_interBank_illiquity_contagion_shock.jl")
+include("../model/stages/fun_stage_interBank_illiquity_allocate.jl")
+include("../model/stages/fun_stage_interBank_illiquity_repay.jl")
+include("../model/stages/fun_stage_exBank_bankrupt_contagion.jl")
+include("../model/stages/fun_stage_interBank_bankrupt_contagion_shock.jl")
+include("../model/stages/fun_stage_bankrupt_repay_shock.jl")
 
 ## 集成过程文件
 include("../model/processes/process_list.jl")
-include("../model/processes/process_exBank_insolvent.jl")
-include("../model/processes/process_interBank_insolvent.jl")
-include("../model/processes/process_exBank_illiquity.jl")
-include("../model/processes/process_interBank_illiquity.jl")
-include("../model/processes/process_exBank_bankrupt.jl")
-include("../model/processes/process_interBank_bankrupt.jl")
+include("../model/processes/fun_process_exBank_insolvent.jl")
+include("../model/processes/fun_process_interBank_insolvent.jl")
+include("../model/processes/fun_process_exBank_illiquity.jl")
+include("../model/processes/fun_process_interBank_illiquity.jl")
+include("../model/processes/fun_process_exBank_bankrupt.jl")
+include("../model/processes/fun_process_interBank_bankrupt.jl")
 
 
 ## 集成模型文件
 include("../model/models/model_list.jl")
-include("../model/models/model_BI1111.jl")
+include("../model/models/fun_model_BI1111.jl")
 
 
 # end # module
