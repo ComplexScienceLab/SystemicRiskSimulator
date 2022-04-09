61c6941bf7984c644a7a679af9c69ab5eeb8c72c
Lingo <ElementsPhoenix@163.com>
2022-04-09 23:59:46 +0800

    mod 2022-04-09
    设计程序模式；

---
+++ src/model/processes/fun_process_skeleton.jl
@@ -0,0 +1,77 @@
+"函数：通用过程框架"
+
+## 函数：通用过程框架
+
+##########################################
+#状态/开发
+##########################################
+
+"""
+通用过程框架：
+Argument: 
+- BB::BankCommercial: 商业银行群变量；
+- BI::BankInterbank: 银行间邻接矩阵变量；
+- para::Dict: 参数变量；
+- env::Dict: 环境变量；
+"""
+function process_interBank_insolvent!(BB::BankCommercial, BI::BankInterbank, para::Dict, env::Dict)
+    ## 过程：资不抵债银行间违约损失传染冲击
+    env[:processName] = "资不抵债银行间违约损失传染冲击过程"
+    @test println("开始过程：$(env[:processName])：")
+
+    env[:indexStage] = 0 # 初始化阶段所在位置
+    env[:isLoop] = true # 初始化循环状态
+    env[:isRound] = true # 初始化回合状态
+    env[:isProcess] = true # 初始化过程状态
+    while env[:isLoop] == true
+
+        env[:tau] += 1 # 回合累加一
+        @test println("开始回合$(env[:tau])")
+
+        ## 设置临时变量
+        BB_Shock_t_t1 = deepcopy(BB.Shock_t)
+        BB_isv_t1 = deepcopy(BB.isv)
+
+        b = TypeState{1}(BB.on .|| BB.off) # 临时设置BB示性变量
+        ib = TypeState{2}((BB.on .|| BB.off) .&& (BB.on .|| BB.off)') # 临时设置BI示性变量
+
+        ## # 资不抵债银行间违约损失冲击阶段
+        env[:stageName] = "资不抵债银行间违约损失冲击阶段"
+        # @test println("阶段：$(env[:stageName])")
+        @scheduler_stage BB, BI = stage_interBank_insolvent_shock!(BB, BI, b, ib, para)
+
+        ## # 资不抵债银行间违约损失传染阶段
+        env[:stageName] = "资不抵债银行间违约损失传染阶段"
+        # @test println("阶段：$(env[:stageName])")
+        @scheduler_stage BB, BI = stage_interBank_insolvent_contagion!(BB, BI, b, ib, para)
+
+
+        # ## 设置临时变量
+        # BB_t1 = deepcopy(BB)
+        # BI_t1 = deepcopy(BI)
+
+        ## TODO存储数据
+        # BB_tau[env[:tau]] = deepcopy(BB) # 存储该回合传染结果数据
+        # BI_tau[env[:tau]] = deepcopy(BI) # 存储该回合传染结果数据
+
+        if BB.isv == BB_isv_t1 # 判定是否结束过程
+            env[:isProcess] = false
+        end
+        isRound!(env) # 判断是否结束回合
+        isLoop!(env) # 判断是否结束循环
+        isJumpOutModel!(env) # 判断是否跳出本次过程
+    
+    end # while
+
+    return BB, BI, env
+
+end # function
+
+
+
+
+
+
+
+
+
