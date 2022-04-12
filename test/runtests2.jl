44467cd082ea84676427362f18f1f3ae0694d4d8
Lingo <ElementsPhoenix@163.com>
2022-04-12 21:38:50 +0800

    Update runtests2.jl

--- test/runtests2.jl
+++ test/runtests2.jl
@@ -7,48 +7,102 @@
 
 using DataFrames
 
-## 定义类型别名
-TypeIds{NDIMS1} = Array{Int16,NDIMS1} # 向量编号类型
-TypeAbbrs{NDIMS1} = Array{String,NDIMS1} # 向量缩写类型
-TypeNames{NDIMS1} = Array{String,NDIMS1} # 向量名称类型
-TypeMoney{NDIMS2} = Array{Float32,NDIMS2} # 向量资金类型
-TypeState{NDIMS2} = Array{Bool,NDIMS2} # 一维向量状态类型
-# EnvironmentVariables = Dict # 环境变量字典类型
-# ParameterVariables = Dict # 参数变量Î字典类型
-# primitive type EnvironmentVariables <: AbstractDict{Any,Any} end
-ItemId = Int8
-ItemName = Symbol
-TypeItem = Dict{ItemId,ItemName}
+include("../src/core/define/define_type.jl")
+include("../src/core/define/define_model.jl")
+include("../src/core/define/define_type.jl")
+include("../src/model/models/model_content.jl")
+include("../src/model/processes/process_content.jl")
+include("../src/model/stages/stage_content.jl")
 
-"定义模型结构体"
-mutable struct Model
-    id::ItemId # 编号 id
-    name::ItemName # 模型名称 name
-    list_process::Vector{Symbol} # 模型需要的过程列表 list_process
-    # list_process::DataFrame # 模型需要的过程列表 list_process
+
+
+# expr=quote
+
+# """
+# 通用模型框架：
+# Argument: 
+# - BB::BankCommercial: 商业银行群变量；
+# - BI::BankInterbank: 银行间邻接矩阵变量；
+# - para::Dict: 参数变量；
+# - env::Dict: 环境变量；
+# """
+# function fun_model_skeleton!(BB::BankCommercial, BI::BankInterbank, para::Dict, env::Dict)
+
+#     env[:indexProcess] = 0 # 初始化过程所在位置
+#     env[:stateOfSchedule] = :stepping
+#     @test println("切换调度运作状态为indexing")
+
+#     env[:tau] = 0 # 初始化回合
+
+#     if (!env[:isModel])
+#         if (env[:tau] > 0)
+#             @test println("继续模型model：\n")
+#         else
+#             @test println("开始模型model：\n")
+#         end
+#     end
+
+    
+#     ## 运行每一个过程
+#     for p in eval(Meta.parse(para[:modelName] * ".listProcess"))
+#         env[:processName] = Symbol(p)
+#         expr = "BB, BI, env = " * String(p) * "!(BB, BI, para, env)"
+#         @scheduler_process Meta.parse(expr)
+#     end
+
+#     ## 判断是否结束
+#     if !env[:isStep]
+#         @test println("步进已结束，跳出$(env[:modelName])。")
+#     end
+
+#     if env[:stateOfSchedule] == :standing
+#         env[:isModel] = false
+#         env[:isExperiment] = false
+#     end
+
+#     if (!env[:isModel] || !env[:isExperiment])
+#         @test println("$(env[:modelName])结束。")
+#     end
+
+
+#     return BB, BI, para, env
+#     # return BB, BI, BB_tau, BI_tau, para, env
+# end # function
+
+# # end # module
+
+# end
+# 
+# 
+
+    # 
+# function model_builder(modelContent::Model; skeleton::Function=fun_model_skeleton!)
+#     print(skeleton) 
+# end
+
+# model_builder(model_BI1111)
+
+function scheduler_indexing(model::Model, stateOfSchedule::Symbol)
+    if stateOfSchedule == :indexing
+        indexOfSchedulePosition = []
+        n=0
+        for (i, p) in enumerate(model.listProcess)
+            append!(indexOfSchedulePosition, [[]])
+            for (j, s) in eval(Meta.parse("enumerate($(p).listStage)"))
+                n+=1
+                push!(indexOfSchedulePosition[i], n)
+            end
+        end
+    end
+    println("indexOfSchedulePosition=$(indexOfSchedulePosition)")
+    stateOfSchedule = :stepping
+    # for i in indexOfSchedulePosition
+    #     println(indexOfSchedulePosition[i])
+    # end
+    return indexOfSchedulePosition, stateOfSchedule
 end
 
-set_model = Set([
-    :process_exBank_insolvent,
-    :process_interBank_insolvent,
-    :process_exBank_illiquity,
-    :process_interBank_illiquity,
-    :process_exBank_bankrupt,
-    :process_interBank_bankrupt,
-])
-
-model_BI1111 = Model(
-    1,
-    :model_BI1111,
-    [
-        :process_exBank_insolvent,
-        :process_interBank_insolvent,
-        :process_exBank_illiquity,
-        :process_interBank_illiquity,
-        :process_interBank_bankrupt
-    ]
-)
 
 
-
+idx,state = scheduler_indexing(model_BI1111,:indexing)
 
