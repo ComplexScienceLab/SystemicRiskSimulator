b94c66902d2901a01e0e444a36d8c60b1596ddea
Lingo <ElementsPhoenix@163.com>
2022-04-15 17:26:48 +0800

    mod 2022-04-15 17:26:34
    思考分析如何生成模型；

--- test/define.jl
+++ test/define.jl
@@ -1,19 +1,14 @@
-"模型生成器"
+# 定义：模型内容结构体
+struct ModelContent
+    content::String
+end
 
-## 模型生成器
-
-##########################################
-#状态/开发
-##########################################
-
-
-"""
-NOW函数：模型生成器
+#= 定义：模型生成器
 做如下事情：
 1. 调取模型核心内容modelContent、模型外围框架modelSkeleton；
 2. 插入模型核心内容modelContent至模型外围框架modelSkeleton内，组合成模型model；
 3. 写出模型model为文件model.jl；
-"""
+=#
 function modelBuilder!(modelContent::ModelContent)
 
     file_modelSkeleton = open("model_skeleton.jl", "r")
@@ -42,13 +37,15 @@ function modelBuilder!(modelContent::ModelContent)
    
 end
 
-
-"""
-NOW过程生成器
-"""
-function process_builder(processContent::ProcessContent; processSkeleton::Function=fun_process_skeleton!)
-
-    return process
+# 定义：运行模型
+macro runModel()
+    expr = quote
+        include("model.jl")
+        model()
+    end
+    println("开始运行模型model：")
+    eval(expr)
+    println("结束运行模型model。")
 end
 
 
