8bd0529d541b60279824106a02885b895eb7f5e5
Lingo <ElementsPhoenix@163.com>
2022-04-16 17:54:16 +0800

    mod 2022-04-16 17:53:50
    实现自动生成模型代码文件的程序；

--- test/model_content.jl
+++ test/model_content.jl
@@ -1,4 +1,38 @@
-# 模型内容实例
-modelContent = ModelContent(
-    """println("我是模型核心内容。")"""
+"变量：模型列表"
+
+## 列出各模型结构体实例
+
+##########################################
+#状态/可扩展
+##########################################
+
+# model_BI1111 = ModelContent(
+#     1, # 编号 id
+#     :model_BI1111, # 函数名称 functionName
+#     "模型BI1111", # 文本名称 textName
+#     [
+#         :process_exBank_insolvent,
+#         :process_interBank_insolvent,
+#         :process_exBank_illiquity,
+#         :process_interBank_illiquity,
+#         :process_interBank_bankrupt
+#     ] # 过程列表 listProcess
+# )
+
+model_BI1111 = ModelContent(
+    1, # 编号 id
+    :model_BI1111, # 函数名称 functionName
+    "模型BI1111", # 文本名称 textName
+    [
+        process_exBank_insolvent,
+        process_interBank_insolvent,
+        process_exBank_illiquity,
+        process_interBank_illiquity,
+        process_interBank_bankrupt
+    ] # 过程列表 listProcess
 )
+
+
+
+
+
