81457701931a2e416b85a4be3630122dae62c900
Lingo <ElementsPhoenix@163.com>
2022-04-18 00:48:11 +0800

    mod 2022-04-17
    测试如何实现组合模型外围框架和核心内容，以读写文件方式；

--- test/test20220415.jl
+++ test/test20220415.jl
@@ -4,7 +4,7 @@ include("define.jl")
 include("process_content.jl")
 include("model_content.jl")
 # include("model_skeleton.jl")
-include("model_builder.jl")
+include("model_exporter.jl")
 # include("fun_model_skeleton_template.jl")
 
 ## 生成模型model
