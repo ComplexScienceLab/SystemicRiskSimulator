d47735562cd8624829684ed87c5c8fb0439c5756
Lingo <ElementsPhoenix@163.com>
2022-05-09 20:20:42 +0800

    mod 2022-05-09 20:20:20
    建模数据收集器；

--- src/core/controller/fun_collector.jl
+++ src/core/controller/fun_collector.jl
@@ -10,9 +10,10 @@
 TODO函数：初始化待收集数据容器
 """
 function initAgentDataCollection(A::SystemicRiskAgent)
-    dict01 = Dict([(:id, RANGE1), (:tau, ONES1), (:indexStage, ONES1)])
-    dict02 = struct2dict(A.BB)
-    dict_fields = Dict([collect(dict01); collect(dict02)])
+    # dict01 = Dict([(:id, collect(range(1, env[:numBank], step=1))), (:tau, ones(env[:numBank])), (:indexStage, ones(env[:numBank]))])
+    # dict02 = struct2dict(A.BB)
+    # dict_fields = Dict([collect(dict01); collect(dict02)])
+    dict_fields = merge(Dict([(:tau, ones(env[:numBank])), (:indexStage, ones(env[:numBank]))]),struct2dict(A.BB))
     # BB = StructArray([BB for i = 1:env[:maxNumOfTau]]) # 初始化带回合变量的商业银行实例数组
     data_BB = DataFrame(dict_fields) # 初始化带回合变量的商业银行实例数据框
     data_BI = StructArray([A.BI for i = 1:env[:maxNumOfTau]]) # 初始化带回合变量的银行间市场实例数组
@@ -22,17 +23,19 @@ end
 
 
 """
-TODO函数：收集数据并存储
+TODO函数：收集数据并存储 #NOW 调度收集数据
 """
 function collector(A::SystemicRiskAgent, A_data::AgentDataCollection; env::Dict=env)
     data_BB = DataFrame()
     # data_BB[!, :tau] = fill(env[:tau], length(fieldnames(typeof(s))))
-    data_BB[!, :id] = RANGE1
-    data_BB[!, :tau] = fill(env[:tau], length(fieldnames(typeof(A.BB))))
-    data_BB[!, :indexStage] = fill(env[:indexStage], length(fieldnames(typeof(A.BB))))
-    agents = collect(values([A.BB]))
-    for field in fieldnames(typeof(A.BB))
-        data_BB[!, field] = collect(getproperty(agent) for agent in agents)
+    # data_BB[!, :id] = collect(range(1, env[:numBank], step=1))
+    data_BB[!, :tau] = fill(env[:tau], env[:numBank])
+    data_BB[!, :indexStage] = fill(env[:indexStage], env[:numBank])
+    # agentsFields = fieldnames(typeof(A.BB))[4:end]
+    # agentsFields = collect(keys(struct2dict(A.BB)))
+    # agentsValues = collect(values(struct2dict(A.BB)))
+    for (k,v) in struct2dict(A.BB)
+        data_BB[!, k] = v
     end
     append!(A_data.BB, data_BB) # 收集banks之数据为一数据框
     A_data.BI[env[:tau]] = A.BI # 收集BI之数据为一结构体向量
