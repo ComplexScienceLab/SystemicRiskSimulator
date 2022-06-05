1ed69ed5ed669561db34e0b4661b97e1546f34dc
Lingo <ElementsPhoenix@163.com>
2022-06-06 00:05:38 +0800

    mod 2022-06-05 18:00:00
    编辑模型框架模板；
    编辑模型框架文档；
    编辑模型导出功能；

--- src/core/template/fun_exporter.jl
+++ src/core/template/fun_exporter.jl
@@ -3,7 +3,7 @@
 ## 模型导出器
 
 ##########################################
-#状态/暂缓开发。等整个程序稳定了再说。
+#状态/开发
 ##########################################
 
 
@@ -16,7 +16,7 @@
 Argument: 
 - processContent::ProcessContent: 过程核心内容
 """
-function processExporter!(processContent::ModelContent; filepath_processSkeleton::String="src/core/template/fun_process_skeleton_template.jl")
+function exportProcess!(processContent::ModelContent; filepath_processSkeleton::String="src/core/template/fun_process_skeleton_template.jl")
 
     ## 读取模型外围框架部分
     file_modelSkeleton = open(filepath_modelSkeleton, "r")
@@ -29,7 +29,7 @@ function processExporter!(processContent::ModelContent; filepath_processSkeleton
     println("替换前的模型：\n" * string_model * "\n")
 
     ## 依次替换文本内容
-    re010 = "fun_model_skeleton!" # 替换模型名称
+    re010 = "fun_model_skeleton_template!" # 替换模型名称
     string_model = replace(string_model, re010 => String(modelContent.functionName) * "!")
     # re020 = "#=【插入过程列表】=#"
     # target020 = """enumerate(para[:modelName] * ".listProcess")"""
@@ -71,7 +71,7 @@ end
 Argument: 
 - modelContent::ModelContent: 模型核心内容
 """
-function modelExporter!(modelContent::ModelContent; filepath_modelSkeleton::String="test/fun_model_skeleton_template.jl")
+function exportModel!(modelContent::ModelContent; filepath_modelSkeleton::String="test/fun_model_skeleton_template.jl")
 
     ## 读取模型外围框架部分
     file_modelSkeleton = open(filepath_modelSkeleton, "r")
--- src/include/SystemicRiskSimulation.jl
+++ src/include/SystemicRiskSimulation.jl
@@ -50,6 +50,7 @@ include("../core/controller/fun_runner.jl")
 include("../core/controller/fun_collector.jl")
 include("../core/controller/fun_agentModel.jl")
 include("../core/controller/fun_makesim.jl")
+include("../core/template/fun_exporter.jl")
 
 ## 集成初始化函数文件
 include("../core/initialization/fun_initVariables.jl")
