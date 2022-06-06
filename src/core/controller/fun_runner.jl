fb6f88892f6a6c6685c90dfd66bc5baa0413e348
Lingo <ElementsPhoenix@163.com>
2022-06-06 15:53:23 +0800

    Update fun_runner.jl
    更新函数说明；

--- src/core/controller/fun_runner.jl
+++ src/core/controller/fun_runner.jl
@@ -7,43 +7,43 @@
 ##########################################
 
 """
-TODO模型运行器。
+模型运行器。
 输入参数将被直接修改。
 
 Argument: 
-- BB::BankCommercial: 商业银行群变量；
-- BI::BankInterbank: 银行间邻接矩阵变量；
+- A::SystemicRiskAgent: Agent群变量；
 - para::Dict: 参数变量；
 - env::Dict: 环境变量；
 - modelComponent::ModelComponent: 模型组件实例；
+- A_data::AgentDataCollection: Agent群变量之数据；
 
 Return:
-- BB::BankCommercial: 商业银行群变量；
-- BI::BankInterbank: 银行间邻接矩阵变量；
+- A::SystemicRiskAgent: Agent群变量；
 - para::Dict: 参数变量；
 - env::Dict: 环境变量；
+- A_data::AgentDataCollection: Agent群变量之数据；
 """
 function runModel!(A::SystemicRiskAgent, para::Dict, env::Dict, modelComponent::ModelComponent, A_data::AgentDataCollection)
-    return A_data, para, env, A_data = modelComponent.run(A, para, env, modelComponent, A_data)
+    return A, para, env, A_data = modelComponent.run(A, para, env, modelComponent, A_data)
 end
 
 
 """
-TODO过程运行器。
+过程运行器。
 输入参数将被直接修改。
 
 Argument: 
-- BB::BankCommercial: 商业银行群变量；
-- BI::BankInterbank: 银行间邻接矩阵变量；
+- A::SystemicRiskAgent: Agent群变量；
 - para::Dict: 参数变量；
 - env::Dict: 环境变量；
 - processComponent::ProcessComponent: 过程组件实例；
+- A_data::AgentDataCollection: Agent群变量之数据；
 
 Return:
-- BB::BankCommercial: 商业银行群变量；
-- BI::BankInterbank: 银行间邻接矩阵变量；
+- A::SystemicRiskAgent: Agent群变量；
 - para::Dict: 参数变量；
 - env::Dict: 环境变量；
+- A_data::AgentDataCollection: Agent群变量之数据；
 """
 function runProcess!(A::SystemicRiskAgent, para::Dict, env::Dict, processComponent::ProcessComponent, A_data::AgentDataCollection)
     return A, para, env, A_data = processComponent.run(A, para, env, processComponent, A_data)
@@ -57,15 +57,11 @@ end
 Argument: 
 - BB::BankCommercial: 商业银行群变量；
 - BI::BankInterbank: 银行间邻接矩阵变量；
+- b::TypeState{1}: 商业银行群示性向量；
+- ib::TypeState{2}: 银行间邻接矩阵示性矩阵；
 - para::Dict: 参数变量；
 - env::Dict: 环境变量；
 - stageComponent::StageComponent: 阶段组件实例；
-
-Return:
-- BB::BankCommercial: 商业银行群变量；
-- BI::BankInterbank: 银行间邻接矩阵变量；
-- para::Dict: 参数变量；
-- env::Dict: 环境变量；
 """
 function runStage!(BB::BankCommercial, BI::BankInterbank, b::TypeState{1}, ib::TypeState{2}, para::Dict, env::Dict, stageComponent::StageComponent)
     stageComponent.run(BB, BI, b, ib, para, env)
