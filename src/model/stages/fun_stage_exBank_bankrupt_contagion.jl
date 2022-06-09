45aa2dc51f553dd560d8f1510dd47df1584da769
Lingo <ElementsPhoenix@163.com>
2022-06-09 13:48:34 +0800

    mod 2022-06-09 13:48:10
    更换原有stages系列函数之参数

--- src/model/stages/fun_stage_exBank_bankrupt_contagion.jl
+++ src/model/stages/fun_stage_exBank_bankrupt_contagion.jl
@@ -5,12 +5,12 @@
 ##########################################
 
 #TODO"函数：外生破产银行间挤兑流动冲击阶段"
-function stage_exBank_bankrupt_contagion!(BB::BankCommercial, BI::BankInterbank, b::TypeState{1}, ib::TypeState{2}, para::Dict, env::Dict)
+function stage_exBank_bankrupt_contagion!(A::SystemicRiskAgent, b::TypeState{1}, ib::TypeState{2}, para::Dict, env::Dict)
     ## # 外生破产银行间挤兑流动传染
     # env[:stageName] = "外生破产银行间挤兑流动冲击阶段"
     @testprintln "开始阶段$(env[:stageName])："
     
-    BB.br[para[:list_Shock_exBI_t]] = para[:Shock_exBI_t][para[:list_Shock_exBI_t]]
+    A.BB.br[para[:list_Shock_exBI_t]] = para[:Shock_exBI_t][para[:list_Shock_exBI_t]]
     update_B_state!(BB, BI; to="bankrupt", from="any")
 
     @testprintln "结束阶段$(env[:stageName])。"
