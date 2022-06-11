e2b4f06a5b2059298da2fdb79b7467d24cb03eab
Lingo <ElementsPhoenix@163.com>
2022-06-12 00:28:26 +0800

    Update __init__.py
    添加包SytemicRisk之init文件；

---
+++ SystemicRisk/__init__.py
@@ -0,0 +1,92 @@
+
+
+
+"系统性风险仿真模拟"
+
+## 系统性风险仿真模拟
+
+##########################################
+# #状态.可扩展
+# 可引入新文件
+##########################################
+
+import os
+from enum import Enum
+
+from scripts.include.includpe_exp_files import *
+
+## 集成定义文件
+from .core.define.define_type import *
+from .core.define.define_enum import *
+from .core.define.define_content import *
+from .core.define.define_component import *
+from .core.define.define_environmentVariables import *
+from .core.define.define_agents import *
+from .core.define.define_parameterVariables import *
+from .core.define.define_agentDataCollection import *
+
+## 集成定义常数文件
+from .core.define.define_consts import *
+
+## 集成工具功能文件
+from .core.controller.fun_tools import *
+
+## 集成调度功能文件
+from .core.controller.fun_io import *
+from .core.controller.fun_schedulers import *
+from .core.controller.fun_builder import *
+from .core.controller.fun_runner import *
+from .core.controller.fun_collector import *
+from .core.controller.fun_agentModel import *
+from .core.controller.fun_makesim import *
+# from .core.template.fun_exporter import *
+
+## 集成初始化函数文件
+from .core.initialization.fun_initVariables import *
+
+## 集成功能函数文件
+from .core.functions.fun_balanceSheet import *
+from .core.functions.fun_state import *
+from .core.functions.fun_shock import *
+from .core.functions.fun_loss import *
+from .core.functions.fun_transfer import *
+from .core.functions.fun_measure import *
+
+## 集成通用框架文件
+from .core.model.fun_process_skeleton import *
+from .core.model.fun_model_skeleton import *
+
+
+
+## 集成名称集合文件
+from .model.model_sets.models_sets import *
+from .model.model_sets.processes_sets import *
+from .model.model_sets.stages_sets import *
+
+## 集成阶段文件
+from .model.stages.fun_stage_exBank_insolvent_shock import *
+from .model.stages.fun_stage_interBank_insolvent_shock import *
+from .model.stages.fun_stage_interBank_insolvent_contagion import *
+from .model.stages.fun_stage_exBank_illiquity_shock import *
+from .model.stages.fun_stage_interBank_illiquity_contagion_shock import *
+from .model.stages.fun_stage_interBank_illiquity_allocate import *
+from .model.stages.fun_stage_interBank_illiquity_repay import *
+from .model.stages.fun_stage_exBank_bankrupt_contagion import *
+from .model.stages.fun_stage_interBank_bankrupt_contagion_shock import *
+from .model.stages.fun_stage_bankrupt_repay_shock import *
+from .model.stages.stage_content import *
+
+## 集成过程文件
+from .model.processes.fun_process_exBank_insolvent import *
+from .model.processes.fun_process_interBank_insolvent import *
+from .model.processes.fun_process_exBank_illiquity import *
+from .model.processes.fun_process_interBank_illiquity import *
+from .model.processes.fun_process_exBank_bankrupt import *
+from .model.processes.fun_process_interBank_bankrupt import *
+from .model.processes.process_content import *
+
+
+## 集成模型文件
+from .model.models.fun_model_BI1111 import *
+from .model.models.model_content import *
+
