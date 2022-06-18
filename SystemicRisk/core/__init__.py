4b5953f8abdf5b0f4eae5904884286d85ed33e07
Lingo <ElementsPhoenix@163.com>
2022-06-18 18:20:21 +0800

    mod 2022-06-18 18:20:11
    继续翻译julia为python；

---
+++ SystemicRisk/core/__init__.py
@@ -0,0 +1,52 @@
+##########################################
+# #状态.可扩展
+# 可引入新文件
+##########################################
+
+
+## 集成定义文件
+from .define.define_type import *
+from .define.define_enum import *
+from .define.define_content import *
+from .define.define_component import *
+from .define.define_environment_variables import *
+from .define.define_agents import *
+from .define.define_parameterVariables import *
+from .define.define_agentDataCollection import *
+
+## 集成定义常数文件
+from .define.define_consts import *
+
+## 集成工具功能文件
+from .controller.fun_tools import *
+
+## 集成调度功能文件
+from .controller.fun_io import *
+from .controller.fun_schedulers import *
+from .controller.fun_builder import *
+from .controller.model_runner import *
+from .controller.fun_collector import *
+from .controller.fun_agentModel import *
+from .controller.fun_makesim import *
+
+## 集成管理功能文件
+from .manager.model_manager import *
+from .manager.agents_manager import *
+
+## 集成模板功能文件
+# from .core.template.fun_exporter import *
+
+## 集成初始化函数文件
+from .initialization.fun_initVariables import *
+
+## 集成功能函数文件
+from .functions.fun_balanceSheet import *
+from .functions.fun_state import *
+from .functions.fun_shock import *
+from .functions.fun_loss import *
+from .functions.fun_transfer import *
+from .functions.fun_measure import *
+
+## 集成通用框架文件
+from .model.fun_process_skeleton import *
+from .model.fun_model_skeleton import *
--- SystemicRisk/core/controller/fun_builder.py
+++ SystemicRisk/core/controller/fun_builder.py
@@ -1,96 +1,98 @@
 "生成器"
 
 ##########################################
-#状态/使用
+# 状态/使用
 ##########################################
+from SystemicRisk.core import ModelContent, ProcessContent, StageContent, ModelComponent, ProcessComponent, StageComponent, fun_model_skeleton, fun_process_skeleton
+
 
 class ModelBuilder:
-
     """
     函数：阶段实例生成器
 
     Argument:
     - stageContent:StageContent: 阶段核心内容；
-    - stageSkeleton:Function = fun_stage_skeleton!: 阶段架构函数；
+    - stageSkeleton:Function = fun_stage_skeleton: 阶段架构函数；
 
     Return:
     - stage:StageComponent: 阶段组件实例
     """
-    def buildStage(stageContent:StageContent):
+
+    def build_stage(stage_content: StageContent):
         ## 获得阶段类型
-        stageInstanceType = Symbol(stageContent.function_name)
+        stageInstanceType = stage_content.function_name
 
         ## 生成阶段stage
-        stage = StageComponent{stageInstanceType}(
-            stageContent.id,
-            stageContent.function_name,
-            stageContent.text_name,
-            stageContent.modelFunction,
+        stage = StageComponent
+        {stageInstanceType}(
+            stage_content.id,
+            stage_content.function_name,
+            stage_content.text_name,
+            stage_content.model_function,
         )
 
-        @testprintln "已经生成阶段$(stageContent.functionName)"
+        # @testprintln "已经生成阶段$(stageContent.functionName)"
 
         return stage
         pass
-
-
 
     """
     函数：过程实例生成器
     
     Argument: 
     - processContent:ProcessContent: 过程核心内容；
-    - processSkeleton:Function = fun_process_skeleton!: 过程架构函数；
+    - processSkeleton:Function = fun_process_skeleton: 过程架构函数；
     
     Return:
     - process:ProcessComponent: 过程组件实例
     """
-    def buildProcess(processContent:ProcessContent, processSkeleton:Function=fun_process_skeleton):
+
+    def buildProcess(process_content: ProcessContent, processSkeleton = fun_process_skeleton):
         ## 获得过程类型
-        processInstanceType = Symbol(processContent.function_name)
+        processInstanceType = process_content.function_name
 
         ## 生成子阶段组件列表
         list_stage = []
-        for stageContent in processContent.listStageContent
+        for stageContent in process_content.listStageContent:
             stage = buildStage(stageContent)
             append(list_stage, [stage])
             pass
 
         ## 生成过程process
-        process = ProcessComponent{processInstanceType}(
-            processContent.id,
-            processContent.function_name,
-            processContent.text_name,
+        process = ProcessComponent
+        {processInstanceType}(
+            process_content.id,
+            process_content.function_name,
+            process_content.text_name,
             # processContent.conditionToContinueProcess,
             processSkeleton,
             list_stage,
         )
 
-        @testprintln "已经生成过程$(processContent.functionName)"
+        # @testprintln "已经生成过程$(processContent.functionName)"
 
         return process
         pass
-
-
 
     """
     函数：模型实例生成器
     
     Argument: 
     - modelContent:ModelContent: 模型核心内容；
-    - modelSkeleton:Function = fun_model_skeleton!: 模型架构函数；
+    - modelSkeleton:Function = fun_model_skeleton: 模型架构函数；
     
     Return:
     - model:ModelComponent: 模型组件实例
     """
-    def buildModel(modelContent:ModelContent, modelSkeleton:Function=fun_model_skeleton):
+
+    def buildModel(modelContent: ModelContent, modelSkeleton = fun_model_skeleton):
 
         ## 获得模型类型
-        modelInstanceType = Symbol(modelContent.function_name)
+        modelInstanceType = modelContent.function_name
 
         ## 生成子过程组件列表
         list_process = []
-        for processContent in modelContent.listProcessContent
+        for processContent in modelContent.listProcessContent:
             process = buildProcess(processContent)
             append(list_process, [process])
             pass
@@ -104,9 +106,9 @@ class ModelBuilder:
             list_process,
         )
 
-        @testprintln "已经生成模型$(modelContent.functionName)"
+        # @testprintln "已经生成模型$(modelContent.functionName)"
 
         return model
         pass
 
-    pass # class
+    pass  # class
