5f179615a4fcfbdc37864e0c5e7c4975c6870d6f
Lingo <ElementsPhoenix@163.com>
2022-06-22 22:26:26 +0800

    mod 2022-06-22 22:25:57
    分别重命名关键词from与to为source与target；

--- src/model/stages/fun_stage_bankrupt_repay_shock.jl
+++ src/model/stages/fun_stage_bankrupt_repay_shock.jl
@@ -9,7 +9,7 @@ function stage_bankrupt_repay_shock!(BB::BankCommercial, BI::BankInterbank, b::T
     ## # 破产银行遭受偿还冲击
     @testprintln "开始阶段$(env[:stage_name])："
 
-    update_B_state!(BB, BI; to="bankrupt", from="any")
+    update_B_state!(BB, BI; target="bankrupt", source="any")
     BB.Shock_D_run_t[BB.br] = BB.Zm_D[BB.br] # 计算破产银行遭受偿还居民存款冲击
     BB.Shock_BI_t[BB.br] = BB.Z_BI_all[BB.br] # 计算破产银行遭受偿还银行间负债冲击
 
