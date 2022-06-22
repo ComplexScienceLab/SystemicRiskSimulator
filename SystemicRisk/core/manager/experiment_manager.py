6013c1eacc8575f10a9e3c6af93ab03f2560033a
Lingo <ElementsPhoenix@163.com>
2022-06-22 22:20:46 +0800

    mod 2022-06-22 22:20:44
    继续翻译julia为python；

--- SystemicRisk/core/manager/experiment_manager.py
+++ SystemicRisk/core/manager/experiment_manager.py
@@ -4,12 +4,13 @@
 @Date   : 2022/06/17
 @Desc   : 
 """
+from SystemicRisk.core import ModelComponent,StateOfScheduleEnum,ModelSetter as init
 
 
 class ExperimentManager:
 
     "函数：运行一次仿真"
-    function makesim(model:ModelComponent, para:dict, env:dict)
+    def makesim(model:ModelComponent, para:dict, env:dict):
 
         ## 初始化agent及其模型
         A, M, A_data = init_systemicRiskAgent(para, env)
@@ -17,8 +18,8 @@ class ExperimentManager:
 
         ##BUG 测试具体模型。
         maxnum = 0
-        env['state_of_process'] = StateOfSchedule.running
-        while env['is_model'] == True && maxnum <= 20: #HACK可能需要设置最大次数maxnum
+        env['state_of_process'] = StateOfScheduleEnum.running
+        while env['is_model'] == True & maxnum <= 20: #HACK可能需要设置最大次数maxnum
             maxnum += 1
             systemicRiskAgent_step(A, M, para, env, model, A_data)
             # return BB, BI, A_data.BB, A_data.BI, env
@@ -27,7 +28,7 @@ class ExperimentManager:
 
         ##BUG 测试Agents框架
         # maxnum = 0
-        # while env['is_model'] == True && maxnum <= 20:
+        # while env['is_model'] == True & maxnum <= 20:
         #     maxnum += 1
         #     step(systemicRiskModel, systemicRiskAgent_step, env['step_size'])
         #     # _, _ = run(systemicRiskModel, systemicRiskAgent_step, 1)
