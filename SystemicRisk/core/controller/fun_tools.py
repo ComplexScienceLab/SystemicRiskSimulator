087ca5128b38842bc2145121813a945404086227
Lingo <ElementsPhoenix@163.com>
2022-06-24 21:44:39 +0800

    mod 2022-06-24
    继续翻译julia为python；

--- SystemicRisk/core/controller/fun_tools.py
+++ SystemicRisk/core/controller/fun_tools.py
@@ -3,13 +3,13 @@
 ## 函数区：工具集
 
 ##########################################
-#状态/开发
+# 状态/开发
 ##########################################
 
-from SystemicRisk.core import itertools
+from SystemicRisk.core import itertools, env
 
-class tools:
 
+class Tools:
 
     # TODO"宏：当测试时使用"
     # macro testprintln(content):
@@ -22,22 +22,29 @@ class tools:
     #         pass
     #     pass
 
+    def test_println(self, content):
+        if env['is_test']:
+            expr = \
+                """
+                write
+                """
 
-    def dict_to_product_list(self,d:dict):
+        pass
+
+    def dict_to_product_list(self, d: dict):
         """
         各字典之列表型元素转列表，其元素为字典，列表个元素间关系符合笛卡尔积。
         :param self:
         :param d:dict:参与转换的字典，字典之每个值都是列表。
         :return: pdl:list: 笛卡尔积字典列表 product lict list；
         """
-        t=list(d.values())
-        l=list(itertools.product(*t,repeat=1))
-        pdl=[]
+        t = list(d.values())
+        l = list(itertools.product(*t, repeat=1))
+        pdl = []
         for v in l:
-            pdl.append(dict(zip(d.keys(),v)))
+            pdl.append(dict(zip(d.keys(), v)))
             pass
         return pdl
         pass
-
 
     pass
--- scripts/exp_raw/main.py
+++ scripts/exp_raw/main.py
@@ -13,27 +13,21 @@ import numpy as np
 from enum import Enum
 import pandas as pd
 
-
 ## 导入相关文件及其内容
 import SystemicRisk as sr
 from scripts.include.includpe_exp_files import *
 # from SystemicRisk.include.SystemicRiskSimulation import *
 # from SystemicRisk.include.Models import *
 
-from SystemicRisk.core import dict_to_product_list,setOfValuesOfParameterVariables,BankState
 
-
-
-
-
+from SystemicRisk.core import Tools, RunModel, ModelSchedulers, setOfValuesOfParameterVariables, BankState, ModelBuilder, StateOfScheduleEnum as S
+from SystemicRisk.model import modelContent_BI1111
 
 os.getcwd()
 
 root_path = os.getcwd()
 
 if __name__ == "__main__":
-
-
 
     ## 设定参数组合
 
@@ -44,42 +38,40 @@ if __name__ == "__main__":
     env = sr.set_experiments_folders()
 
     ## 建立文件以记录log
-    f = open(os.path.join(env['folderpath_of_experiments_output_data'], "outputlog.txt"),"w")
-
+    f = open(os.path.join(env['folderpath_of_experiments_output_data'], "outputlog.txt"), "w")
 
     # @testprintln "\n实验组名称：$(env['foldername_of_experiments'])"
 
     ## 设置字典列表，由setOfParametersValues各参数之各可能的取值排列组合而成。此将用于做实验
-    list_combinationOfPara = dict_to_product_list(setOfValuesOfParameterVariables)  # 组合排列多结构体成为列表
+    list_combinationOfPara = Tools.dict_to_product_list(setOfValuesOfParameterVariables)  # 组合排列多结构体成为列表
     env['num_experiment'] = len(list_combinationOfPara)  # 获取实验组之实验个数
-    df_combinationOfPara = pd.DataFrame(np.asarray(list_combinationOfPara),columns=setOfValuesOfParameterVariables.keys())  # 转换字典列表为数据框
-    df_combinationOfPara.insert(loc=0,column='exp_id',value=np.repeat(list(range(1,env['num_experiment']+1)),repeats=env['num_bank'],axis=0)) # 添加实验组id
-    df_combinationOfPara.insert(loc=0,column='id',value=list(range(1,len(df_combinationOfPara)+1)),axis=0) # 添加数据项id
-    df_combinationOfPara.to_csv(os.path.join(env['folderpath_of_experiments_output_data'],"paras.csv"),df_combinationOfPara) # 导出字段列表为csv格式
+    df_combinationOfPara = pd.DataFrame(np.asarray(list_combinationOfPara), columns=setOfValuesOfParameterVariables.keys())  # 转换字典列表为数据框
+    df_combinationOfPara.insert(loc=0, column='exp_id', value=np.repeat(list(range(1, env['num_experiment'] + 1)), repeats=env['num_bank'], axis=0))  # 添加实验组id
+    df_combinationOfPara.insert(loc=0, column='id', value=list(range(1, len(df_combinationOfPara) + 1)), axis=0)  # 添加数据项id
+    df_combinationOfPara.to_csv(os.path.join(env['folderpath_of_experiments_output_data'], "paras.csv"), df_combinationOfPara)  # 导出字段列表为csv格式
 
     ## 初始化参数变量
     # @testprintln "\n列出所有实验组："
-    for (idx_para, para) in enumerate(list_combinationOfPara)
-        # @testprintln
-        "$(idx_para): $(para);"
-        modelContent = eval(Meta.parse("modelContent_" * para['model_name']))
+    for (idx_para, para) in enumerate(list_combinationOfPara):
+        # @testprintln "$(idx_para): $(para);"
+        modelContent = eval("modelContent_" + para['model_name'])
         pass
 
     ## 构建本次实验组所需的所有模型
-    model = buildModel(modelContent_BI1111)  # 根据基准模型BI1111预先初始化model变量
-    if length(setOfValuesOfParameterVariables['model_name']) > 1
-        for model_name in setOfValuesOfParameterVariables['model_name'][2:    pass]
-            modelContent = eval(Meta.parse("modelContent_$(model_name)"))
+    model = ModelBuilder.buildModel(modelContent_BI1111)  # 根据基准模型BI1111预先初始化model变量
+    if len(setOfValuesOfParameterVariables['model_name']) > 1:
+        for model_name in setOfValuesOfParameterVariables['model_name'][1:-1]:
+            modelContent = eval("modelContent_" + model_name)
             # if True: # FIXME如果不存在模型文件，则构建模型
-            model = buildModel(modelContent)
+            model = ModelBuilder.buildModel(modelContent)
             #     pass
             pass
         pass
 
-    println("\n实验组开始：\n")
+    print("\n实验组开始：\n")
 
     ## 主循环
-    for (i, para) in enumerate(list_combinationOfPara)
+    for (i, para) in enumerate(list_combinationOfPara):
         env['id_experiment'] = i  # 设定当前实验编号
         # 重置环境变量
         env['index_of_schedule_position'] = []
@@ -105,40 +97,29 @@ if __name__ == "__main__":
         env['is_rocess'] = True
         env['is_model'] = True
         env['is_experiment'] = True
-        env['state_of_schedule'] =StateOfSchedule.indexing
-        env['state_of_process'] =:initializing
+        env['state_of_schedule'] = S.indexing
+        env['state_of_process'] = S.idle
 
         ## 生成模型内容
         # modelComponent = eval(Meta.parse(para['model_name']))
 
         ## 调度：生成位置索引
-        if env['state_of_schedule'] ==: indexing
-        env['index_of_schedule_position'], env['state_of_schedule'] = scheduler_indexing(model)
-        pass
+        if env['state_of_schedule'] == S.indexing:
+            env['index_of_schedule_position'], env['state_of_schedule'] = ModelSchedulers.scheduler_indexing(model)
+            pass
 
+        # @testprintln    "\n实验$(env['id_experiment'])/$(length(list_combinationOfPara))开始："
 
-    @testprintln
+        # @testprintln    "\n相关实验参数：$(para)"
 
+        ## 进行实验
 
-    "\n实验$(env['id_experiment'])/$(length(list_combinationOfPara))开始："
+        RunModel.makesim(model, para, env)
 
-
-    @testprintln
-
-
-    "\n相关实验参数：$(para)"
-
-    ## 进行实验
-
-    makesim(model, para, env)
-
-
-    @testprintln
-
-
-    "本次实验结束，还剩下$(length(list_combinationOfPara)-env['id_experiment'])个实验。\n"
+        # @testprintln    "本次实验结束，还剩下$(length(list_combinationOfPara)-env['id_experiment'])个实验。\n"
         pass  # for
 
-    println("实验组结束。")
+    print("实验组结束。")
 
-    close(f)
+    f.close()
+    pass # main
