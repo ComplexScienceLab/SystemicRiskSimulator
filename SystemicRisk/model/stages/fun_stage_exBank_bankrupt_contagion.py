35904fadd472dab76a23234f72948e5b9946c628
Lingo <ElementsPhoenix@163.com>
2022-06-28 17:38:35 +0800

    mod 2022-06-28 17:38:25
    继续翻译julia为python；

--- SystemicRisk/model/stages/fun_stage_exBank_bankrupt_contagion.py
+++ SystemicRisk/model/stages/fun_stage_exBank_bankrupt_contagion.py
@@ -1,19 +1,22 @@
 ## 函数：外生破产银行间挤兑流动冲击阶段
 
 ##########################################
-#状态/暂时用不到
+# 状态/暂时用不到
 ##########################################
 
-#TODO"函数：外生破产银行间挤兑流动冲击阶段"
-def stage_exBank_bankrupt_contagion(self, BB:BankCommercial, BI:BankInterbank, b:TypeState, ib:TypeState, para:dict, env:dict):
+from SystemicRisk.core import TypeState, BankInterbank, BankCommercial, BankState
+
+
+# TODO"函数：外生破产银行间挤兑流动冲击阶段"
+def stage_exBank_bankrupt_contagion(self, BB: BankCommercial, BI: BankInterbank, b: TypeState, ib: TypeState, para: dict, env: dict):
     ## # 外生破产银行间挤兑流动传染
     # env['stage_name'] = "外生破产银行间挤兑流动冲击阶段"
     # @testprintln "开始阶段$(env['stage_name'])："
-    
+
     BB.br[para['list_Shock_exBI_t']] = para['Shock_exBI_t'][para['list_Shock_exBI_t']]
-    update_B_state(BB, BI; to="bankrupt", from="any")
+    BankState.update_B_state(BB, BI, target="bankrupt", source="any")
 
     # @testprintln "结束阶段$(env['stage_name'])。"
     return BB, BI
 
-    pass # functions
+    pass  # functions
