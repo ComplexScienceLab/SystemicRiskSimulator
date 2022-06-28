578b6c02b3ea7436364dac200b86a64f1bcb4a4e
Lingo <ElementsPhoenix@163.com>
2022-06-28 17:38:35 +0800

    mod 2022-06-28 17:38:25
    继续翻译julia为python；

--- SystemicRisk/model/stages/fun_stage_interBank_insolvent_shock.py
+++ SystemicRisk/model/stages/fun_stage_interBank_insolvent_shock.py
@@ -13,7 +13,7 @@ def stage_interBank_insolvent_shock(self, BB:BankCommercial, BI:BankInterbank, b
     @. BB.A_BI_all[b] = max(BB.A_BI_all[b] - BB.Shock_def_t[b], 0.0) # 银行之银行间资产变动
     update_B_balanceSheet(BB, BI, b, ib; byWay="A_BI_all")
     @. BB.E_all[BB.on] = max(BB.E_all[BB.on] - BB.Shock_def_t[BB.on], 0.0) # 银行之所有者权益变动
-    update_B_state(BB, BI; to="insolvent", from="healthy") # 更新各银行之状态，从健康到资不抵债
+    update_B_state(BB, BI; target = "insolvent", source = "healthy") # 更新各银行之状态，从健康到资不抵债
     @. BB.Shock_BI_def_s[BB.isv] = abs((BB.Shock_def_t[BB.isv] - BB.E_all[BB.isv]) / (BB.Z_BI_all[BB.isv] + BB.Z_D[BB.isv]) * BB.Z_BI_all[BB.isv]) # 计算应银行内冲击传导至银行间传染冲击
     update_B_Shock(BB, BI, b, ib; byWay="Shock_BI_def_s") # 更新违约损失冲击源头变量Shock_def_s
     @. BB.Shock_D_def_s[BB.isv] = abs((BB.Shock_def_t[BB.isv] - BB.E_all[BB.isv]) / (BB.Z_BI_all[BB.isv] + BB.Z_D[BB.isv]) * BB.Z_D[BB.isv]) # 计算应银行内冲击传导至银行存款传染冲击
