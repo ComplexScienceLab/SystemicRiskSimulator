97b600a2806c102a729a0917e9a9353b5a54b1fa
Lingo <ElementsPhoenix@163.com>
2022-06-30 21:25:27 +0800

    mod 2022-06-30 21:25:27
    继续翻译julia为python；

--- SystemicRisk/core/controller/fun_io.py
+++ SystemicRisk/core/controller/fun_io.py
@@ -3,7 +3,7 @@
 ## 函数区：输入输出流
 
 ##########################################
-#状态/使用
+# 状态/使用
 ##########################################
 import os.path
 
@@ -16,10 +16,10 @@ import os.path
 - env:EnvironmentVariables: 环境变量，此时内部内容已经被更新；
 """
 
-from SystemicRisk import time,env
+from SystemicRisk.core import time, env
 
-def set_experiments_folders(self, env:dict=env, isDatetime:bool=True):
 
+def set_experiments_folders(self, env: dict = env, isDatetime: bool = True):
     ## 设定日期时间字符串
     if isDatetime == True:
         str_datetime = "_" * time.strftime("%Y%m%d%H%M%S")
@@ -39,13 +39,10 @@ def set_experiments_folders(self, env:dict=env, isDatetime:bool=True):
     env['foldername_of_experiments'] = str_manuallyName * str_datetime
     env['folderpath_of_experiments'] = os.path.join(env['root_dir_of_experiments'], env['foldername_of_experiments'])
 
-    os.mkdir(env['folderpath_of_experiments']) # 创建文件夹
+    os.mkdir(env['folderpath_of_experiments'])  # 创建文件夹
     env['folderpath_of_experiments_output_data'] = os.path.join(env['folderpath_of_experiments'], env['foldername_of_experiments_output_data'])
     # cd("$(env['folderpath_of_experiments'])")
-    os.mkdir(env['folderpath_of_experiments_output_data']) # 创建文件夹，以导出实验输出数据
+    os.mkdir(env['folderpath_of_experiments_output_data'])  # 创建文件夹，以导出实验输出数据
 
     return env
-    pass # functioin
-
-
-
+    pass  # functioin
