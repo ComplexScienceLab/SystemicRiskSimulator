683d882bda74f9679e60b9389f1b83d59f576fbe
Lingo <ElementsPhoenix@163.com>
2022-06-30 17:48:41 +0800

    mod 2022-06-30 17:48:33
    继续翻译julia为python；

--- draft/draft2022-06-23.py
+++ draft/draft2022-06-23.py
@@ -5,13 +5,20 @@
 @Desc   : 
 """
 import itertools
-import numpy as np,pandas as pd
+import numpy as np, pandas as pd
 
-d=dict(a=['a1','a2'],b=['b1','b2'],c=['c1'])
-l=list(d.values())
-product_list=list(itertools.product(*l,repeat=1))
-pdl=[]
-for i,v in enumerate(product_list):
-    pdl.append(dict(zip(d.keys(),v)))
-npdl=np.asarray(product_list)
-df_product_list=pd.DataFrame(npdl,columns=d.keys())
+
+class class1:
+    d = dict(a=['a1', 'a2'], b=['b1', 'b2'], c=['c1'])
+    l = list(d.values())
+    pass
+
+
+product_list = list(itertools.product(*class1.l, repeat=1))
+pdl = []
+for i, v in enumerate(product_list):
+    pdl.append(dict(zip(class1.d.keys(), v)))
+npdl = np.asarray(product_list)
+df_product_list = pd.DataFrame(npdl, columns=class1.d.keys())
+
+class1.d['dataBB']={'d1':[1,2,3],'d2':[1,1,1],'d3':[0,0,0]}
