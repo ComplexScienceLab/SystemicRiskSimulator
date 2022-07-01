df852786156dd9b073e0cbccd8571c8d143e49bb
Lingo <ElementsPhoenix@163.com>
2022-07-01 10:58:46 +0800

    mod 2022-07-01 10:58:40
    排除错误；

--- SystemicRisk/core/controller/fun_io.py
+++ SystemicRisk/core/controller/fun_io.py
@@ -18,31 +18,34 @@ import os.path
 
 from SystemicRisk.core import time, env
 
+class ModelIO:
 
-def set_experiments_folders(self, env: dict = env, isDatetime: bool = True):
-    ## 设定日期时间字符串
-    if isDatetime == True:
-        str_datetime = "_" * time.strftime("%Y%m%d%H%M%S")
-    else:
-        str_datetime = ""
-        pass
+    def set_experiments_folders(self, env: dict = env, isDatetime: bool = True):
+        ## 设定日期时间字符串
+        if isDatetime == True:
+            str_datetime = "_" * time.strftime("%Y%m%d%H%M%S")
+        else:
+            str_datetime = ""
+            pass
 
-    ## 设定前缀字符串
-    if env['foldername_type_of_experiments'] == "default":
-        str_manuallyName = "default"
-    elif env['foldername_type_of_experiments'] == "set manually":
-        str_manuallyName = env['foldername_prefix_of_experiments']
-    else:
-        raise Exception("关键词取值错误！".format(env['foldername_type_of_experiments']))
-        pass
+        ## 设定前缀字符串
+        if env['foldername_type_of_experiments'] == "default":
+            str_manuallyName = "default"
+        elif env['foldername_type_of_experiments'] == "set manually":
+            str_manuallyName = env['foldername_prefix_of_experiments']
+        else:
+            raise Exception("关键词取值错误！".format(env['foldername_type_of_experiments']))
+            pass
 
-    env['foldername_of_experiments'] = str_manuallyName * str_datetime
-    env['folderpath_of_experiments'] = os.path.join(env['root_dir_of_experiments'], env['foldername_of_experiments'])
+        env['foldername_of_experiments'] = str_manuallyName * str_datetime
+        env['folderpath_of_experiments'] = os.path.join(env['root_dir_of_experiments'], env['foldername_of_experiments'])
 
-    os.mkdir(env['folderpath_of_experiments'])  # 创建文件夹
-    env['folderpath_of_experiments_output_data'] = os.path.join(env['folderpath_of_experiments'], env['foldername_of_experiments_output_data'])
-    # cd("$(env['folderpath_of_experiments'])")
-    os.mkdir(env['folderpath_of_experiments_output_data'])  # 创建文件夹，以导出实验输出数据
+        os.mkdir(env['folderpath_of_experiments'])  # 创建文件夹
+        env['folderpath_of_experiments_output_data'] = os.path.join(env['folderpath_of_experiments'], env['foldername_of_experiments_output_data'])
+        # cd("$(env['folderpath_of_experiments'])")
+        os.mkdir(env['folderpath_of_experiments_output_data'])  # 创建文件夹，以导出实验输出数据
 
-    return env
-    pass  # functioin
+        return env
+        pass  # functioin
+
+    pass # class
