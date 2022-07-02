ccd0bf094690074961eebd008fe58be4f4f77647
Lingo <ElementsPhoenix@163.com>
2022-07-02 16:24:17 +0800

    mod 2022-07-02 16:24:16
    解决导入问题；

--- SystemicRisk/core/define/define_consts.py
+++ SystemicRisk/core/define/define_consts.py
@@ -3,12 +3,15 @@
 ## 程序：定义常量
 
 ##########################################
-#状态/无用
+# 状态/无用
 ##########################################
 
-from SystemicRisk.core import np,env
+# from SystemicRisk.core import np, env
 pass  # end import
 
+import numpy as np
+from SystemicRisk.core.define.define_environment_variables import env
+pass  # end import
 
 
 # const FALSE1 = np.full(env['num_bank'],False) # 一维false布尔向量常量
@@ -19,8 +22,8 @@ pass  # end import
 # const BLANK2 = fill("", env['num_bank'], env['num_bank']) # 一维方阵空字符串向量常量
 # const ZEROS1 = np.zeros(env['num_bank']) # 一维零向量常量
 # const ZEROS2 = np.zeros((env['num_bank'], env['num_bank'])) # 二维方阵零向量常量
-LESS1 = np.zeros(env['num_bank']) + 0.01 # 一维接近零的正数向量常量
-LESS2 = np.zeros((env['num_bank'], env['num_bank'])) + 0.1 # 二维方阵接近零的正数常量
+LESS1 = np.zeros(env['num_bank']) + 0.01  # 一维接近零的正数向量常量
+LESS2 = np.zeros((env['num_bank'], env['num_bank'])) + 0.1  # 二维方阵接近零的正数常量
 # const ONES1 = np.ones(env['num_bank']) # 一维幺向量常量
 # const ONES2 = np.ones(env['num_bank'], env['num_bank']) # 二维方阵幺向量常量
 # const NOTHING1 = fill(nothing, env['num_bank']) # 一维缺失值向量常量
