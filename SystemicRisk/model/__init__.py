b6bda554afae91586c64c4e4df4811a9a90bd340
Lingo <ElementsPhoenix@163.com>
2022-07-09 13:37:32 +0800

    mod 2022-07-09 13:37:30
    解决导入问题；

--- SystemicRisk/model/__init__.py
+++ SystemicRisk/model/__init__.py
@@ -3,31 +3,11 @@
 # 可引入新文件
 ##########################################
 
-## 集成阶段文件
-from .stages.fun_stage_exBank_insolvent_shock import *
-from .stages.fun_stage_interBank_insolvent_shock import *
-from .stages.fun_stage_interBank_insolvent_contagion import *
-from .stages.fun_stage_exBank_illiquity_shock import *
-from .stages.fun_stage_interBank_illiquity_contagion_shock import *
-from .stages.fun_stage_interBank_illiquity_allocate import *
-from .stages.fun_stage_interBank_illiquity_repay import *
-from .stages.fun_stage_exBank_bankrupt_contagion import *
-from .stages.fun_stage_interBank_bankrupt_contagion_shock import *
-from .stages.fun_stage_bankrupt_repay_shock import *
-from .stages.stage_content import *
-pass  # end import
-
-
-
-
-## 集成过程文件
-from .processes.process_content import *
-
-## 集成模型文件
-from .models.fun_model_BI1111 import *
-from .models.model_content import *
+import SystemicRisk
 
 ## 集成名称集合文件
 from .model_sets.models_sets import *
 from .model_sets.processes_sets import *
 from .model_sets.stages_sets import *
+
+pass  # end import
--- SystemicRisk/model/stages/__init__.py
+++ SystemicRisk/model/stages/__init__.py
@@ -1,5 +1,14 @@
 
 
-# from SystemicRisk.model.stages import *
-# from SystemicRisk.core import TypeState, BankInterbank, BankCommercial,BankState
-
+## 集成阶段文件
+from SystemicRisk.model.stages.fun_stage_exBank_insolvent_shock import *
+from SystemicRisk.model.stages.fun_stage_interBank_insolvent_shock import *
+from SystemicRisk.model.stages.fun_stage_interBank_insolvent_contagion import *
+from SystemicRisk.model.stages.fun_stage_exBank_illiquity_shock import *
+from SystemicRisk.model.stages.fun_stage_interBank_illiquity_contagion_shock import *
+from SystemicRisk.model.stages.fun_stage_interBank_illiquity_allocate import *
+from SystemicRisk.model.stages.fun_stage_interBank_illiquity_repay import *
+from SystemicRisk.model.stages.fun_stage_exBank_bankrupt_contagion import *
+from SystemicRisk.model.stages.fun_stage_interBank_bankrupt_contagion_shock import *
+from SystemicRisk.model.stages.fun_stage_bankrupt_repay_shock import *
+from SystemicRisk.model.stages.stage_content import *
