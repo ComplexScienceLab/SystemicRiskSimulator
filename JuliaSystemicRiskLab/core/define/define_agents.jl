d571a93c13b818ae73510f135fb27555c0090b89
Lingo <ElementsPhoenix@163.com>
2022-12-28 17:20:48 +0800

    Squashed commit of the following:
    commit ab345c689c4c63b6bcea437b1e43a38e8bf7f51e
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Wed Dec 28 17:20:17 2022 +0800
    
        rename 2022-12-28 17:20:16
    
        重命名`illiquity`为`illiquid`，包括文件夹名、文件名、变量名、函数名、注释；
    
    commit 112c73e070396bc5ac5106212976fd0d813eb8a3
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Wed Dec 28 17:06:20 2022 +0800
    
        rename 2022-12-28 17:05:12
    
        重命名`illiquity`为`illiquid`，包括文件夹名、文件名、变量名、函数名、注释；
    
    commit 6e143b38d155d7ebec216e34d09b4d274cda1048
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Sat Nov 12 17:50:01 2022 +0800
    
        Update fun_builder.jl
    
        “生成”改成“安装”；
    
    commit 9cb6277e08dbfb902ed8c85d3ed0c8bb050901fc
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Fri Nov 11 21:36:02 2022 +0800
    
        Update define_agents.jl
    
        修复错误；
    
    commit 1520447a44d9292e7e23baadc0ec4f63076188c5
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Fri Nov 11 21:25:31 2022 +0800
    
        mod 2022-11-11 21:25:30
    
        重命名agent之编号id与data之编号id；
    
    commit 6c0df14d49cad0599668cc90378abfdf69cab26f
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Fri Nov 11 21:03:39 2022 +0800
    
        manage 2022-11-11 21:03:37
    
        中文名为“器”的，改成“机”；
    
    commit ed66239a96770b5d47d4c91d13b28a1cf720da0f
    Merge: adac873 06cfb5c
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Mon Oct 24 23:23:57 2022 +0800
    
        Merge branch 'dev_julia' of gitee.com:EthanLingo/SystemicRisk into dev_julia
    
    commit adac873df80738a9550f8893b231eacbeefe803f
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Tue Oct 11 21:38:27 2022 +0800
    
        manage 2022-10-11
    
        修改Julia版本号；
    
    commit 684c56e8ea2798a57d64ae5551797223e46c44dc
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Mon Oct 10 21:42:58 2022 +0800
    
        Update set_environmentVariables.jl
    
        排除错误；
    
    commit 5188c9e7800e1c7fe02b2ecffbcf8400f9e16c68
    Author: Lingo <ElementsPhoenix@163.com>
    Date:   Mon Oct 10 21:42:47 2022 +0800
    
        change 2022-10-10
    
        修改Julia版本号；

--- JuliaSystemicRiskLab/core/define/define_agents.jl
+++ JuliaSystemicRiskLab/core/define/define_agents.jl
@@ -76,7 +76,7 @@ mutable struct BankCommercial{NDIMS1,NDIMS2}
     off::TypeState{NDIMS2} # 示性向量之于银行是否已退出不存在 isOff
     hel::TypeState{NDIMS2} # 示性向量之于银行是否健康 isHealthy
     isv::TypeState{NDIMS2} # 示性向量之于银行是否资不抵债 isInsolvent
-    ilq::TypeState{NDIMS2} # 示性向量之于银行是否流动性短缺 isIlliquity
+    ilq::TypeState{NDIMS2} # 示性向量之于银行是否流动性短缺 isIlliquid
     br::TypeState{NDIMS2} # 示性向量之于银行是否破产 isBankrupt
     nBoBI::TypeState{NDIMS2} # 示性向量之于银行是否需要偿还银行间借款 isNeededBoBI
     eBoBI::TypeState{NDIMS2} # 示性向量之于银行是否可以偿还银行间借款 isEnabledBoBI
@@ -87,7 +87,7 @@ mutable struct BankCommercial{NDIMS1,NDIMS2}
     isAllocatedShock::TypeState{NDIMS2} # 示性向量之于银行是否已经分配传染冲击 isAllocatedShock
     listOfExist::TypeList{Any} # 列表之于存在的银行编号 listOfExist
     listOfInsolvent::TypeList{Any} # 列表之于资不抵债的银行编号 listOfInsolvent
-    listOfIlliquity::TypeList{Any} # 列表之于流动性短缺的银行编号 listOfIlliquity
+    listOfIlliquid::TypeList{Any} # 列表之于流动性短缺的银行编号 listOfIlliquid
     listOfBankrupt::TypeList{Any} # 列表之于破产的银行编号 listOfBankrupt
 end
 # isDefault::TypeState{NDIMS2} # 示性向量之于银行是否违约
@@ -118,14 +118,14 @@ mutable struct BankInterbank{NDIMS2}
     off::TypeState{NDIMS2} # 信息邻接矩阵之于银行间已退出不存在的 isOff
     hel::TypeState{NDIMS2} # 信息邻接矩阵之于银行间健康的 isHealthy
     isv::TypeState{NDIMS2} # 信息邻接矩阵之于银行间资不抵债的 isInsolvent
-    ilq::TypeState{NDIMS2} # 信息邻接矩阵之于银行间流动性短缺的 isIlliquity
+    ilq::TypeState{NDIMS2} # 信息邻接矩阵之于银行间流动性短缺的 isIlliquid
     br::TypeState{NDIMS2} # 信息邻接矩阵之于银行间破产的 isBankrupt
     cre::TypeList{Any} # 信息列表之于各银行之债权方银行编号 listOfCreditors
     deb::TypeList{Any} # 信息列表之于各银行之债务方银行编号 listOfDebtors
     cre_isv::TypeList{Any} # 信息列表之于资不抵债的银行之债权方银行编号 listOfCreditorsInInsolvent
     deb_isv::TypeList{Any} # 信息列表之于资不抵债的银行之债务方银行编号 listOfDebtorsInInsolvent
-    cre_ilq::TypeList{Any} # 信息列表之于流动性短缺的银行之债权方银行编号 listOfCreditorsInIlliquity
-    deb_ilq::TypeList{Any} # 信息列表之于流动性短缺的银行之债务方银行编号 listOfDebtorsInIlliquity
+    cre_ilq::TypeList{Any} # 信息列表之于流动性短缺的银行之债权方银行编号 listOfCreditorsInIlliquid
+    deb_ilq::TypeList{Any} # 信息列表之于流动性短缺的银行之债务方银行编号 listOfDebtorsInIlliquid
     cre_br::TypeList{Any} # 信息列表之于破产的银行之债权方银行编号 listOfCreditorsInBankrupt
     deb_br::TypeList{Any} # 信息列表之于破产的银行之债务方银行编号 listOfDebtorsInBankrupt
 end
