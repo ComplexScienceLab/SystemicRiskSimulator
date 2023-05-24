e3b56cf581aadd89d269a67fd768dc1725e6d138
Lingo <ElementsPhoenix@163.com>
2023-05-24 10:24:38 +0800

    Update content_IB1111.py

--- PySystemicRiskLab/models/contents/algorithms_contents/content_IB1111.py
+++ PySystemicRiskLab/models/contents/algorithms_contents/content_IB1111.py
@@ -11,30 +11,28 @@ pass  # end import
 
 # from PySystemicRiskLab.models_entities.algorithms_contents import *
 
-def content_IB1111():  #TODO等需要的时候再写
+def content_IB1111():  # TODO等需要的时候再写
     ## # 算法IB1111
 
     # TODO改成普通的循环形式处理
 
+    # HACK 2023-05-22以前的版本
+    process_START = b.build_process("START")
+    process_ExBankInsolvent = b.build_process("entity_ExBankInsolvent")
+    process_InterBankInsolvent = b.build_process("entity_InterBankInsolvent")
+    process_ExBankIlliquid = b.build_process("entity_ExBankIlliquid")
+    process_InterBankIlliquid = b.build_process("entity_InterBankIlliquid")
+    process_InterBankBankrupt = b.build_process("entity_InterBankBankrupt")
 
+    p.process_processEntity(process_START)
+    p.process_processEntity(process_ExBankInsolvent)
+    condition_01, out_flow_entity = p.process_conditionEntity(process_ExBankInsolvent)
+    for c in condition_01:
+        if c is True:
+            p.process_processEntity("entity_ExBankInsolvent")
 
-    ## HACK 2023-05-22以前的版本
-    # process_00 = b.build_process("START")
-    # process_01 = b.build_process("entity_ExBankInsolvent")
-    # process_02 = b.build_process("entity_InterBankInsolvent")
-    # process_03 = b.build_process("entity_ExBankIlliquid")
-    # process_04 = b.build_process("entity_InterBankIlliquid")
-    # process_05 = b.build_process("entity_InterBankBankrupt")
-    #
-    # p.process_processEntity(process_00)
-    # p.process_processEntity(process_01)
-    # condition_01, out_flow_entity = p.process_conditionEntity(process_01)
-    # for c in condition_01:
-    #     if c is True:
-    #         p.process_processEntity("entity_ExBankInsolvent")
-    #
-    # label .process_02
-    #
-    # entity_InterBankInsolvent
+    label.process_InterBankInsolvent
+
+    entity_InterBankInsolvent
 
     pass  # method
