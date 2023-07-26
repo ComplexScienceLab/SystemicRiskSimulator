a62bfceeb657423874143491d719f39319efa1da
Lingo <ElementsPhoenix@163.com>
2023-07-26 17:41:50 +0800

    Update entityData_IB1111.py
    尝试解决条件错误；

--- PySystemicRiskLab/models/entities_data/entityData_IB1111.py
+++ PySystemicRiskLab/models/entities_data/entityData_IB1111.py
@@ -24,14 +24,14 @@ entityData_IB1111 = dict(
         "node_END": "END",
     }),
     condition=dict({
-        "node_01_condition_01": "(A.BB.isv != A_data.BB[env['round']-1]['dataBB'].isv).any()", # BUG 因为改用`step`，所以可能存在错误
-        "node_01_condition_02": "(A.BB.isv == A_data.BB[env['round']-1]['dataBB'].isv).all()",
-        "node_02_condition_01": "(A.BB.isv != A_data.BB[env['round']-1]['dataBB'].isv).any()",
-        "node_02_condition_02": "(A.BB.isv == A_data.BB[env['round']-1]['dataBB'].isv).all()",
-        "node_03_condition_01": "(A.BB.ilq != A_data.BB[env['round']-1]['dataBB'].ilq).any()",
-        "node_03_condition_02": "(A.BB.ilq == A_data.BB[env['round']-1]['dataBB'].ilq).all()",
-        "node_04_condition_01": "(A.BB.ilq != A_data.BB[env['round']-1]['dataBB'].ilq).any()",
-        "node_04_condition_02": "(A.BB.ilq == A_data.BB[env['round']-1]['dataBB'].ilq).all()",
+        "node_01_condition_01": r"(A.BB.isv != [dd for dd in A_data.BB if dd['round'] == (env['round'] - 1)][-1]['dataBB'].isv).any()",
+        "node_01_condition_02": r"(A.BB.isv == [dd for dd in A_data.BB if dd['round'] == (env['round'] - 1)][-1]['dataBB'].isv).all()",
+        "node_02_condition_01": r"(A.BB.isv != [dd for dd in A_data.BB if dd['round'] == (env['round'] - 1)][-1]['dataBB'].isv).any()",
+        "node_02_condition_02": r"(A.BB.isv == [dd for dd in A_data.BB if dd['round'] == (env['round'] - 1)][-1]['dataBB'].isv).all()",
+        "node_03_condition_01": r"(A.BB.ilq != [dd for dd in A_data.BB if dd['round'] == (env['round'] - 1)][-1]['dataBB'].ilq).any()",
+        "node_03_condition_02": r"(A.BB.ilq == [dd for dd in A_data.BB if dd['round'] == (env['round'] - 1)][-1]['dataBB'].ilq).all()",
+        "node_04_condition_01": r"(A.BB.ilq != [dd for dd in A_data.BB if dd['round'] == (env['round'] - 1)][-1]['dataBB'].ilq).any()",
+        "node_04_condition_02": r"(A.BB.ilq == [dd for dd in A_data.BB if dd['round'] == (env['round'] - 1)][-1]['dataBB'].ilq).all()",
     }),
     content=None,
     node=list([
