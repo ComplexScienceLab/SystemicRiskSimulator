4f57664348a4552dec1389722c76a962d098bf8f
Lingo <ElementsPhoenix@163.com>
2023-11-20 00:18:10 +0800

    mod 2023-11-20 00:18:02

--- SystemicRiskSimulator/core/define/define_environmentVariables.py
+++ SystemicRiskSimulator/core/define/define_environmentVariables.py
@@ -4,7 +4,7 @@
 记得这里发生变更时，要手动检查`Operator.operate_experiment`相关位置是否要更新。
 """
 from SystemicRiskSimulator import os
-from Scripts.settings.set_environment_variables import set_environment_variables
+from Samples.settings.set_environment_variables import set_environment_variables
 from SystemicRiskSimulator.core.define.define_type import EnvironmentVariableType
 from SystemicRiskSimulator.core.define.define_enum import StateOfScheduleEnum
 
