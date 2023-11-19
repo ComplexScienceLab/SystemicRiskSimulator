8daa80a92c08d5fcc8ef92bc7baf01a866aea4d6
Lingo <ElementsPhoenix@163.com>
2023-11-20 00:17:57 +0800

    mod 导入调试样例
    从先前迁出的 Scripts 文件夹导入。

---
+++ Samples/settings/set_parameter_variables.py
@@ -0,0 +1,46 @@
+"程序：设置参数变量parameter_variables"
+
+
+
+from SystemicRiskSimulator import np
+pass  # end import
+
+set_parameter_variables = dict(
+
+    ######### 设置参数变量 #########################################
+
+    ## 指定待处理的模型
+    model_name=[
+        'IB1111',
+    ],
+
+    ## 外生违约损失冲击权重百分比
+    Shock_exIB_def_t_percentage=[
+        np.array([1.0, 0.0, 0.0, 0.0, 0.0]),
+        np.array([0.0, 0.0, 0.0, 0.0, 0.0]),
+    ],
+
+    ## 外生挤兑流动冲击权重百分比
+    Shock_exIB_run_t_percentage=[
+        np.array([0.0, 0.0, 0.0, 0.0, 0.0]),
+        np.array([1.0, 0.0, 0.0, 0.0, 0.0]),
+    ],
+
+    ## 指定遭受初始外生冲击的银行列表
+    list_Shock_exIB_t=[
+        np.array([True, False, False, False, False]),
+    ],
+
+    ## 银行抛售厂商贷款资产价格折扣率。默认0.0。#HACK目前暂时不考虑这个参数！
+    kappa_A_P=[
+        0.0
+    ],
+
+    ## 银行抛售银行间贷款资产价格折扣率。默认0.0。#HACK目前暂时不考虑这个参数！
+    kappa_IB=[
+        0.0
+    ],
+
+    ################################################################
+
+)
