d2977002b9a7bf2274f19886b4e2fb647af611f1
Lingo <ElementsPhoenix@163.com>
2023-12-28 14:50:00 +0800

    mod 2023-12-28 14:49:55
    修复一些问题；

--- SystemicRiskSimulator/simulator.py
+++ SystemicRiskSimulator/simulator.py
@@ -25,7 +25,6 @@ def simulator(config: dict):
     ## 获取项目路径、模拟器工具路径
     config['folderpath_project'] = Tools.get_project_rootpath()
     config['folderpath_simulator'] = Tools.get_project_rootpath(config['foldername_simulator'], config['folderpath_realpath_simulator'])
-    # config['foldername_outputData'] = Tools.get_project_rootpath(config['foldername_outputData'], config['folderpath_realpath_outputData'])
     # 如果 settings 之 config 有内容，那么就删除，否则就从其他文件夹中复制之后再导入
     Tools._delete_and_recreate_folder(Path(config['folderpath_simulator'], "SystemicRiskSimulator/data/config"), is_auto_confirmation=config['is_auto_confirmation'])
     Tools._copy_files_from_other_folders(Path(config['folderpath_project'], config['folderpath_config']), Path(config['folderpath_simulator'], "SystemicRiskSimulator/data/config"), is_auto_confirmation=config['is_auto_confirmation'])
