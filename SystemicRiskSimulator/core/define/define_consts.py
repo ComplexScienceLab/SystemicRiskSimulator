b566a47593983f1db53cee4a71479757c464e8ef
Lingo <ElementsPhoenix@163.com>
2023-12-30 20:43:37 +0800

    dev 重新定义 CONST 常量
    RANGE 相关的函数改成从1开始计数；
    
    一维常量不再强制设置为二维列向量的形状；

--- SystemicRiskSimulator/core/define/define_consts.py
+++ SystemicRiskSimulator/core/define/define_consts.py
@@ -7,7 +7,6 @@ pass  # end import
 
 import numpy as np
 
-
 pass  # end import
 
 
@@ -35,31 +34,40 @@ class CONST:
     - MISSING2: 二维方阵确失值常量；
     - NONE1: 一维空向量常量；
     - NONE2: 二维空向量常量；
-    - RANGE1: 一维步进向量常量（从0开始）；
-    - RANGE2: 二维方阵步进向量常量（从0开始）；
+    - RANGE1: 一维步进向量常量（从1开始）；
+    - RANGE2: 二维方阵步进向量常量（从1开始）；
 
     """
 
     @classmethod
     def __init__(cls, num):
-        cls.FALSE1 = np.full((num, 1), False)  # 一维false布尔向量常量
+        # cls.FALSE1 = np.full((num, 1), False)  # 一维false布尔向量常量
+        cls.FALSE1 = np.full(num, False)  # 一维false布尔向量常量
         cls.FALSE2 = np.full((num, num), False)  # 二维方阵false布尔向量常量
-        cls.TRUE1 = np.full((num, 1), True)  # 一维true布尔向量常量
+        # cls.TRUE1 = np.full((num, 1), True)  # 一维true布尔向量常量
+        cls.TRUE1 = np.full(num, True)  # 一维true布尔向量常量
         cls.TRUE2 = np.full((num, num), True)  # 二维方阵true布尔向量常量
+        # cls.BLANK1 = np.full((num, 1), "")  # 一维空字符串向量常量
         cls.BLANK1 = np.full(num, "")  # 一维空字符串向量常量
         cls.BLANK2 = np.full((num, num), "")  # 一维方阵空字符串向量常量
-        cls.ZEROS1 = np.zeros((num, 1))  # 一维零向量常量
+        # cls.ZEROS1 = np.zeros((num, 1))  # 一维零向量常量
+        cls.ZEROS1 = np.zeros(num)  # 一维零向量常量
         cls.ZEROS2 = np.zeros((num, num))  # 二维方阵零向量常量
-        cls.LESS1 = np.zeros((num, 1)) + 0.0001  # 一维接近零的正数向量常量
+        # cls.LESS1 = np.zeros((num, 1)) + 0.0001  # 一维接近零的正数向量常量
+        cls.LESS1 = np.zeros(num) + 0.0001  # 一维接近零的正数向量常量
         cls.LESS2 = np.zeros((num, num)) + 0.0001  # 二维方阵接近零的正数常量
-        cls.ONES1 = np.ones((num, 1))  # 一维幺向量常量
+        # cls.ONES1 = np.ones((num, 1))  # 一维幺向量常量
+        cls.ONES1 = np.ones(num)  # 一维幺向量常量
         cls.ONES2 = np.ones((num, num))  # 二维方阵幺向量常量
-        cls.MISSING1 = np.full((num, 1), np.NaN)  # 一维缺失值向量常量
+        # cls.MISSING1 = np.full((num, 1), np.NaN)  # 一维缺失值向量常量
+        cls.MISSING1 = np.full(num, np.NaN)  # 一维缺失值向量常量
         cls.MISSING2 = np.full((num, num), np.NaN)  # 二维方阵确失值常量
-        cls.NONE1 = np.empty((num, 1), dtype=object)  # 一维空向量常量
+        # cls.NONE1 = np.empty((num, 1), dtype=object)  # 一维空向量常量
+        cls.NONE1 = np.empty(num, dtype=object)  # 一维空向量常量
         cls.NONE2 = np.empty((num, num), dtype=object)  # 二维空向量常量
-        cls.RANGE1 = np.arange(0, num, step=1)  # 一维步进向量常量（从0开始）
-        cls.RANGE2 = np.arange(0, num * num, step=1).reshape((num, num))  # 二维方阵步进向量常量（从0开始）
+        # cls.RANGE1 = np.arange(0, num, step=1).reshape((num, 1))  # 一维步进向量常量（从0开始）
+        cls.RANGE1 = np.arange(1, num + 1, step=1)  # 一维步进向量常量（从1开始）
+        cls.RANGE2 = np.arange(1, num * num + 1, step=1).reshape((num, num))  # 二维方阵步进向量常量（从1开始）
         pass  # end init
 
     pass  # end class
