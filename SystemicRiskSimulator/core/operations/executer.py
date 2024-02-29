bf32481e4ca4bd3028ca2aaaaedb48676de42869
Lingo <ElementsPhoenix@163.com>
2024-02-29 22:23:12 +0800

    dev 继续适配、对接相关模型

--- SystemicRiskSimulator/core/operations/executer.py
+++ SystemicRiskSimulator/core/operations/executer.py
@@ -19,23 +19,31 @@ class Executer:
     """
 
     @classmethod
-    def round_step_update(cls, function, update_way: str, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
+    def round_step_update(cls, function, process_name: str, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
         """
         执行一次轮次级别（轮次粒度）的步进更新。对应强化学习的一次步进更新。
 
         Args:
             function (function): 相关的需要步进更新的功能函数
-            update_way (str): 更新方式
             A (SystemicRiskAgent): 多主体
             A_data (AgentDataCollection): 多主体之数据
             para (dict): 参数集
             sgv (dict): 模拟器全局变量
 
         Returns:
+            A (SystemicRiskAgent): 多主体
+            sgv (dict): 模拟器全局变量
 
         """
-        logging.debug(f"               轮次：{sgv['round']}，过程：{sgv['process']}，更新源：{update_way}")
-        #NOW
+        sgv['round'] += 1  # 计次轮次数（由于开始轮次是`START`，所以记为0）
+        sgv['phase'] = 1  # 逐相复位（起始为1）
+        sgv['process_name'] = process_name
+        logging.debug(f"        轮次：{sgv['round']}，模型：{sgv['process_name']}")
+        A, sgv = function(A, A_data, para, sgv)  # 更新金融变量
+        # NOW
+
+        return A, sgv
+
         pass  # function
 
     ## NOTE：执行一次变量变更级别的步进更新。
@@ -55,11 +63,11 @@ class Executer:
             sgv (dict): 模拟器全局变量
 
         Returns:
-            sgv (dict): 模拟器全局变量
+            None
 
         """
 
-        logging.debug(f"               步进：{sgv['step']}，相：{sgv['phase']}，更新源：{update_way}")
+        logging.debug(f"                步进：{sgv['step']}，相：{sgv['phase']}，更新源：{update_way}")
         # Finance.update_finance_variables(A.BB, A.IB, A.b, A.ib, by_way=update_way)  # 更新金融变量
         function.update_finance_variables(A.BB, A.IB, A.b, A.ib, by_way=update_way)  # 更新金融变量
         Collector.collect_agent_data(A, A_data, sgv)
@@ -67,7 +75,7 @@ class Executer:
         sgv['step'] += 1  # 步进加一
         sgv['phase'] += 1  # 逐相加一
 
-        return sgv
+        # return sgv
         pass  # function
 
     @classmethod
