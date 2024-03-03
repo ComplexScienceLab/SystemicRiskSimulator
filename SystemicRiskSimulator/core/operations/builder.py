6092082105c5448ca5ce0a5a9e7eac3d5f0ed707
Lingo <ElementsPhoenix@163.com>
2024-03-03 21:48:48 +0800

    dev 继续对接、适配强化学习环境工具包

--- SystemicRiskSimulator/core/operations/builder.py
+++ SystemicRiskSimulator/core/operations/builder.py
@@ -30,18 +30,18 @@ class Builder:
         ## 导入模型之初始态实体之数据内容（NOTE 动态导入）
 
         if sgv['is_use_Gymnasium_model']:
-            ## #NOTE 如果使用由强化学习工具包自定义的模型 #DEBUG
-            ### #NOTE 子方案一：自定义的模型内容与基于 Gym 构建的模型分成两个文件 #DEBUG
+            ## #NOTE 如果使用由强化学习环境工具包自定义的模型 #DEBUG
+            ### #NOTE 子方案一：自定义的模型内容与由强化学习环境工具包自定义的环境模型分成两个文件 #DEBUG
             ## 导入实体之内容
             list_entityData = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/entities')), r"entity_", sgv['folderpath_simulator'])
             ## 导入模型之内容（NOTE 动态导入）
-            modelContents = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/contents')), r"model_", sgv['folderpath_simulator'])
-            ## 导入由`Gymnasium`自定义的环境模型（NOTE 动态导入）
-            gymEnvContents = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/gym_env')), r"gym_env_", sgv['folderpath_simulator'])
-            # ### #NOTE 子方案二：自定义的模型内容与基于 Gym 构建的模型合在一个类里面 #DEBUG
+            modelContents = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/contents')), r"content_", sgv['folderpath_simulator'])
+            ## 导入由强化学习环境工具包自定义的环境模型（NOTE 动态导入）
+            modelContents.update(Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/environments')), r"environment_", sgv['folderpath_simulator']))
+            # ### #NOTE 子方案二：自定义的模型内容与由强化学习环境工具包自定义的环境模型合在一个类里面 #DEBUG
             # ## 导入实体之内容
             # list_entityData = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/entities')), r"entity_", sgv['folderpath_simulator'])
-            # ## 导入模型之内容类（由`Gymnasium`自定义的）（NOTE 动态导入）
+            # ## 导入模型之内容类（由强化学习环境工具包自定义的）（NOTE 动态导入）
             # modelContents = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/gym_model')), r"gym_model_", sgv['folderpath_simulator'])
 
             ## 根据模型实体数据列表之数据，生成相应的模型实体对象，然后组成模型实体列表
@@ -49,7 +49,7 @@ class Builder:
                 EntityManager.create_entity(entityData=entityData)
                 pass  # for
         else:
-            ## NOTE 如果使用模拟器自带的模型，不使用由强化学习工具包自定义的模型 #DEBUG
+            ## NOTE 如果使用模拟器自带的模型，不使用由强化学习环境工具包自定义的模型 #DEBUG
             ## 导入实体之内容
             list_entityData = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/entities')), r"entity_", sgv['folderpath_simulator'])
             ## 导入模型之内容（NOTE 动态导入）
@@ -58,6 +58,8 @@ class Builder:
             for entityData in list_entityData.values():
                 EntityManager.create_entity(entityData=entityData)  # 根据实体数据，创建每个实体
                 pass  # for
+
+            pass  # if
 
         # 补充模型实体之特征
         # for modelEntity in EntityManager.modelEntities.values():
@@ -84,7 +86,7 @@ class Builder:
                 pass  # if
             pass  # for
 
-        ## 装配模型实体之执行器：将模型实体之执行器之值链接至对应的模型内容功能函数
+        ## 装配模型实体之执行器、内容器之值链接至对应的模型内容功能函数
         for modelEntity in EntityManager.modelEntities.values():
             if (
                     modelEntity.attribute.entity_type == {"template entity"} and
@@ -106,6 +108,7 @@ class Builder:
             mainModelInstanceEntity_name = mainModelTemplateEntity.attribute.entity_name
             mainModelInstanceEntity = EntityManager.create_entity(entity_name='model_' + mainModelInstanceEntity_name, entity_type={"instance entity"}, structure_type={"container structure", "process structure"}, container_type={"root container"}, process_type={"executive process"}, content_type={"model content", "node content"})  # 构造一个空实体
             mainModelInstanceEntity.content = mainModelTemplateEntity  # 设置模型实例实体之内容组件为主模型实体
+            pass  # for
 
         pass  # function
 
--- SystemicRiskSimulator/core/operations/operator.py
+++ SystemicRiskSimulator/core/operations/operator.py
@@ -65,15 +65,8 @@ class Operator:
             Builder.build_entities_by_process_and_container_component(sgv)  # NOTE：一次只处理一个模型 #HACK 已经过时，可以删除
         else:
             ## NOTE 如果直接使用非流程版的形式的模型
-            if sgv['is_use_Gymnasium_model']:
-                ## #NOTE 如果使用由强化学习工具包自定义的模型 #DEBUG
-                Builder.build_entities_by_execute(sgv) #NOW
-                pass  # if
-            else:
-                Builder.build_entities_by_execute(sgv)
-                ## NOTE 如果使用模拟器自带的模型，不使用由强化学习工具包自定义的模型 #DEBUG
-                pass  # if
-            pass  # if
+            Builder.build_entities_by_execute(sgv)
+        pass  # if
 
         return sgv, EntityManager.mainModelInstanceEntities
 
@@ -129,10 +122,10 @@ class Operator:
             ## NOTE 如果直接使用非流程版的形式的模型。HACK 注意这个时候 `env['test_max_num_of_round']` 失效
 
             if sgv['is_use_Gymnasium_model']:
-                ## #NOTE 如果使用由强化学习工具包自定义的模型 #DEBUG
+                ## #NOTE 如果使用由强化学习环境工具包自定义的模型 #DEBUG
                 pass  # if #NOW
             else:
-                ## NOTE 如果使用模拟器自带的模型，不使用由强化学习工具包自定义的模型 #DEBUG
+                ## NOTE 如果使用模拟器自带的模型，不使用由强化学习环境工具包自定义的模型 #DEBUG
 
                 # Scheduler.schedule(sgv)  # 调度状态变成`running`
 
