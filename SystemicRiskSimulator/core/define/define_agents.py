0aa385d155a31a2df8832e1b8b7c0bfde2655785
Lingo <ElementsPhoenix@163.com>
2024-03-12 21:29:31 +0800

    dev 添加预定义策略

--- SystemicRiskSimulator/core/define/define_agents.py
+++ SystemicRiskSimulator/core/define/define_agents.py
@@ -132,6 +132,7 @@ class BankCommercial(BaseAgents):  # TODO有必要改成动态创建类属性
     # list_illiquid = np.NaN  # = np.full((sgv['num_bank'], 1), list)  # 列表之于流动性短缺的银行编号 list_illiquid
     # list_bankrupt = np.NaN  # = np.full((sgv['num_bank'], 1), list)  # 列表之于破产的银行编号 list_bankrupt
     # TODO 增加监管约束之状态；
+
     rewards = np.NaN,  # 各银行之奖励 rewards （类似效用函数）
 
     def __init__(self, *args, **kwargs):
@@ -174,6 +175,8 @@ class BankInterbank(BaseInterAgents):
     isv = np.NaN  # 信息邻接矩阵之于银行间资不抵债的 is_insolvent
     ilq = np.NaN  # 信息邻接矩阵之于银行间流动性短缺的 is_illiquid
     br = np.NaN  # 信息邻接矩阵之于银行间破产的 is_bankrupt
+
+    theta_IB_def = np.NaN  # 银行间资产负债违约分配比例 theta_IB_def
 
     # cre = np.NaN  # 信息列表之于各银行之债权方银行编号 list_creditors
     # deb = np.NaN  # 信息列表之于各银行之债务方银行编号 list_debtors
