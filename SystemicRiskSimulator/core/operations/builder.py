ba162aab18e60bc7965fca7d43720b3d3da53722
Lingo <ElementsPhoenix@163.com>
2024-03-21 17:48:13 +0800

    mod 2024-03-21 17:47:53

--- SystemicRiskSimulator/core/operations/builder.py
+++ SystemicRiskSimulator/core/operations/builder.py
@@ -36,11 +36,10 @@ class Builder:
             list_entityData = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/entities')), r"entity", sgv['folderpath_simulator'])
             ## 导入模型之内容（NOTE 动态导入）
             modelContents = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/contents')), r"content_", sgv['folderpath_simulator'])
-            ## 导入由强化学习环境工具包自定义的环境模型（NOTE 动态导入）
-            modelEnvironment = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/env')), r"environment", sgv['folderpath_simulator'])
-            ## 导入由强化学习环境工具包自定义的运行过程（NOTE 动态导入）
             ## 导入使用强化学习环境工具包自定义的环境模型（NOTE 动态导入）
+            modelEnvironment = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/env')), r"Environment", sgv['folderpath_simulator'])
             ## 导入使用强化学习算法工具包自定义的算法模型（NOTE 动态导入）
+            modelAlgorithm = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/env')), r"Train", sgv['folderpath_simulator'])
             ## 导入使用强化学习环境工具包自定义的运行过程（NOTE 动态导入）
             modelProcess = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/process')), r"process", sgv['folderpath_simulator'])
             # ### #NOTE 子方案二：自定义的模型内容与使用强化学习环境工具包自定义的环境模型合在一个类里面 #HACK 这个方案暂时无用，也没有实现。
