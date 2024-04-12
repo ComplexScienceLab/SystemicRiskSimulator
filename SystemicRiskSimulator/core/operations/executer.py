a5b987bc03672ced0820b3ec9e7192ccd72b4701
Lingo <ElementsPhoenix@163.com>
2024-04-12 21:18:29 +0800

    rename 2024-04-12 21:18:05

--- SystemicRiskSimulator/core/operations/executer.py
+++ SystemicRiskSimulator/core/operations/executer.py
@@ -18,12 +18,12 @@ class Executer:
     """
 
     # @classmethod
-    # def turn_step_update(cls, model_content, turn: int, process_name: str, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
+    # def turn_step_update(cls, content, turn: int, process_name: str, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
     #     """
     #     执行一次轮次级别（轮次粒度）的步进更新。对应强化学习的一次步进更新。
     #
     #     Args:
-    #         model_content (class): 相关的需要步进更新的功能类
+    #         content (class): 相关的需要步进更新的功能类
     #         A (SystemicRiskAgent): 多主体
     #         A_data (AgentDataCollection): 多主体之数据
     #         para (dict): 参数集
@@ -38,23 +38,23 @@ class Executer:
     #     sgv['phase'] = 1  # 逐相复位（起始为1）
     #     sgv['process_name'] = process_name
     #     logging.debug(f"        轮次：{sgv['turn']}，模型：{sgv['process_name']}")
-    #     A, sgv = model_content.content_model_process(A, A_data, para, sgv)  # 执行一次轮次级别的步进更新
-    #     # A, sgv = model_content.execute_model_content(A, A_data, para, sgv)  # 执行一次轮次级别的步进更新
+    #     A, sgv = content.content_model_process(A, A_data, para, sgv)  # 执行一次轮次级别的步进更新
+    #     # A, sgv = content.execute_model_content(A, A_data, para, sgv)  # 执行一次轮次级别的步进更新
     #
     #     return A, sgv
     #
     #     pass  # function
 
     # @classmethod
-    # def turn_step_update(cls, model_content, process_name: str, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
+    # def turn_step_update(cls, content, process_name: str, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
 
     @classmethod
-    def turn_step_update(cls, model_content, A: SystemicRiskAgent, A_last: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
+    def turn_step_update(cls, content, A: SystemicRiskAgent, A_last: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
         """
         #NOTE：执行一次轮次级别（轮次粒度）的步进更新。对应强化学习的一次步进更新。
 
         Args:
-            model_content (class): 相关的需要步进更新的功能类
+            content (object): 相关的需要步进更新的功能类或者实例
             A (SystemicRiskAgent): 多主体
             A_last (SystemicRiskAgent): 上一回合的多主体
             A_data (AgentDataCollection): 多主体之数据
@@ -70,7 +70,7 @@ class Executer:
         sgv['phase'] = 1  # 逐相复位（起始为1）
         # sgv['process_name'] = process_name
         logging.debug(f"        轮次：{sgv['turn']}，模型：{sgv['process_name']}")
-        model_content.model_content(A, A_last, A_data, para, sgv)  # 执行一次轮次级别的步进更新
+        content.model_content(A, A_last, A_data, para, sgv)  # 执行一次轮次级别的步进更新
 
         # 深拷贝一份作为上一回合的数据
         A_last = SystemicRiskAgent(2, deepcopy(A.BB), deepcopy(A.b), deepcopy(A.IB), deepcopy(A.ib))
@@ -80,14 +80,14 @@ class Executer:
         pass  # function
 
     @classmethod
-    def variable_step_update(cls, model_content, update_way: str, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
+    def variable_step_update(cls, content, update_way: str, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
         """
         #NOTE：执行一次变量变更级别的步进更新
 
         更新方式具体见：`Finance.update_variables` 对应的[文档](SystemicRiskSimulator/core/functions/content_finance.py)。
 
         Args:
-            model_content (function): 相关的需要步进更新的功能函数
+            content (object): 相关的需要步进更新的功能类或者实例
             update_way (str): 更新方式
             A (SystemicRiskAgent): 多主体
             A_data (AgentDataCollection): 多主体之数据
@@ -101,7 +101,7 @@ class Executer:
 
         logging.debug(f"                步进：{sgv['step']}，相：{sgv['phase']}，更新源：{update_way}")
         # Finance.update_variables(A.BB, A.IB, A.b, A.ib, by_way=update_way)  # 更新金融变量
-        model_content.update_variables(A.BB, A.IB, A.b, A.ib, by_way=update_way)  # 更新金融变量
+        content.update_variables(A.BB, A.IB, A.b, A.ib, by_way=update_way)  # 更新金融变量
         if sgv['RL_state'] is 'using':
             Collector.collect_agent_data(A, A_data, sgv)
             pass  # if
@@ -111,6 +111,37 @@ class Executer:
 
         # return sgv
         pass  # function
+
+    # @classmethod
+    # def agentsRewards_variable_step_update(cls, content, update_way: str, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict):
+    #     """
+    #     #NOTE：执行一次个体众奖励函数值变更级别的步进更新
+    #
+    #     Args:
+    #         content (object): 相关的需要步进更新的功能类或者实例
+    #         update_way (str): 更新方式
+    #         A (SystemicRiskAgent): 多主体
+    #         A_data (AgentDataCollection): 多主体之数据
+    #         para (dict): 参数集
+    #         sgv (dict): 模拟器全局变量
+    #
+    #     Returns:
+    #         None
+    #
+    #     """
+    #
+    #     logging.debug(f"                步进：{sgv['step']}，相：{sgv['phase']}，更新源：{update_way}")
+    #     # Finance.update_variables(A.BB, A.IB, A.b, A.ib, by_way=update_way)  # 更新金融变量
+    #     content.update_variables(A.BB, A.IB, A.b, A.ib, by_way=update_way)  # 更新金融变量
+    #     if sgv['RL_state'] is 'using':
+    #         Collector.collect_agent_data(A, A_data, sgv)
+    #         pass  # if
+    #
+    #     sgv['step'] += 1  # 步进加一
+    #     sgv['phase'] += 1  # 逐相加一
+    #
+    #     # return sgv
+    #     pass  # function
 
     @classmethod
     def execute_main_model_entity(cls, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict, entity: Entity):
