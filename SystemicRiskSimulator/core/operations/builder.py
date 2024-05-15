7581925b93aa82d6b728e54b8db4bfac8ad564c2
Lingo <ElementsPhoenix@163.com>
2024-05-15 12:03:19 +0800

    fix 修复带有 PettingZoo 、Gym 等的环境模型 IB41111

--- SystemicRiskSimulator/core/operations/builder.py
+++ SystemicRiskSimulator/core/operations/builder.py
@@ -110,8 +110,9 @@ class Builder:
             ## 导入模型之内容（NOTE 动态导入）
             modelContents = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/contents')), r"[Cc]ontent_", sgv['folderpath_simulator'])
             ## 导入使用强化学习环境工具包自定义的环境模型（NOTE 动态导入）
-            # modelEnvironment = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/env')), r"Environment|Train", sgv['folderpath_simulator'])
-            modelEnvironment = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/env')), r"environment", sgv['folderpath_simulator'])
+            modelEnvironment = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/contents')), r"[Ee]nvironment", sgv['folderpath_simulator'])
+            ## 导入使用强化学习环境工具包自定义的运行过程（NOTE 动态导入）
+            modelProcess = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/process')), r"process", sgv['folderpath_simulator'])
             # ## 导入使用强化学习算法工具包自定义的算法模型（NOTE 动态导入）
             # modelAlgorithm = Tools.import_modules_from_package(str(Path(sgv['folderpath_simulator'], r'SystemicRiskSimulator/data/models/algorithm')), r"train", sgv['folderpath_simulator'])
             # ## 导入使用强化学习环境工具包自定义的运行过程（NOTE 动态导入）
@@ -162,7 +163,8 @@ class Builder:
                         modelEntity.attribute.content_type == {"model content"}
                 ):
                     modelEntity.content = modelContents  # 设置内容器之值是具体的模型相关的功能函数
-                    modelEntity.environment = modelEnvironment[modelEntity.environment]  # 设置环境之值是具体的模型相关的功能函数
+                    modelEntity.process = modelProcess[modelEntity.process]  # 设置运作器之值是具体的模型相关的功能函数
+                    # modelEntity.environment = modelEnvironment[modelEntity.environment]  # 设置环境之值是具体的模型相关的功能函数
                     # modelEntity.algorithm = modelAlgorithm[modelEntity.algorithm]  # 设置算法之值是具体的模型相关的功能函数
                     # modelEntity.process = modelProcess[modelEntity.process]  # 设置运作器之值是具体的模型相关的功能函数
                     pass  # if
