017968a50a285eee34097d15b281a6993bd1a145
Lingo <ElementsPhoenix@163.com>
2024-05-20 18:03:54 +0800

    rename 2024-05-20 18:02:55

--- SystemicRiskSimulator/core/operations/operator.py
+++ SystemicRiskSimulator/core/operations/operator.py
@@ -332,7 +332,7 @@ class Operator:
     @classmethod
     def operate_step_experiment(cls, A: SystemicRiskAgent, A_data: AgentDataCollection, sgv: dict, para: dict, model: Any):
         """
-        运作步进实验。用于使用使用强化学习环境工具包自定义的模型。
+        运作步进实验。用于使用强化学习环境工具包自定义的模型。
 
         Args:
             A (SystemicRiskAgent): 多主体
