043d67f68b10465b2d854d4e65cd84f43c4cee2f
Lingo <ElementsPhoenix@163.com>
2024-09-15 23:13:09 +0800

    fix 修复可视化程序打印日志问题

--- SystemicRiskSimulator/simulator.py
+++ SystemicRiskSimulator/simulator.py
@@ -162,7 +162,6 @@ def simulator(config: dict):
         log_console_handler = logging.StreamHandler()
         logger.addHandler(log_console_handler)
 
-
         if not sgv['is_develope_mode']:
             sgv_pkl = pickle.dumps(sgv)
             sgv_base64 = base64.b64encode(sgv_pkl).decode('utf-8')
@@ -197,13 +196,21 @@ def simulator(config: dict):
         Tools.delete_and_recreate_folder(sgv['folderpath_experiments_output_config'], is_auto_confirmation=config['is_auto_confirmation'])  # 删除输出文件夹原来的 config 文件夹
         Tools.copy_files_from_other_folders(sgv['folderpath_config'], sgv['folderpath_experiments_output_config'], is_auto_confirmation=sgv['is_auto_confirmation'])  # 导出 config 文件夹到输出文件夹
 
-        # logger = logging.getLogger()
+        logger = logging.getLogger()
         # logger.setLevel(sgv['test_logging'])
-        #
-        # log_file_handler = logging.FileHandler(Path(sgv['folderpath_experiments_output_log'], "outputlog.txt"))
-        # logger.addHandler(log_file_handler)
+        logger.setLevel('ERROR')  # #BUG 因为可视化程序出现大量的无意义的输出，需要后续解决。目前暂时屏蔽。
+
+        log_file_handler = logging.FileHandler(Path(sgv['folderpath_experiments_output_log'], "outputlog.txt"))
+        logger.addHandler(log_file_handler)
         # log_console_handler = logging.StreamHandler()
         # logger.addHandler(log_console_handler)
+
+        ## 关闭日志记录器
+        log_file_handler.close()
+        logger.removeHandler(log_file_handler)
+        # log_console_handler.close()
+        # logger.removeHandler(log_console_handler)
+        # del log_file_handler, log_console_handler, logger
 
         if not sgv['is_develope_mode']:
             sgv_pkl = pickle.dumps(sgv)
