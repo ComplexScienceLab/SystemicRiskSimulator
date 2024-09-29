b6342006ec2cf832ffe32e4be74144589d7da027
Lingo <ElementsPhoenix@163.com>
2024-09-29 10:13:18 +0800

    fix 尝试修复报错问题
    原因在修改模型之后，计算结果当中，数组元素出现 NaN 值。修改转换程序保留 NaN 值解决。但是绘图仍然会出现错误。

--- SystemicRiskSimulator/programs/transform_output_data_program.py
+++ SystemicRiskSimulator/programs/transform_output_data_program.py
@@ -338,8 +338,8 @@ def fun_导入Pandas格式的实验结果数据转换为面板形式再导出(ex
     df_BB_panel = df_BB.explode('id_agent')  # 只展开 'id_agent' 列
     for col in list_columns_for_explode:  # 遍历其他需要展开的列，并将它们的元素展开以匹配 'id_agent' 列的行数
         if col != 'id_agent':
-            # df_BB_panel[col] = df_BB.apply(lambda row: pd.Series(row[col]), axis=1).stack().reset_index(level=0, drop=True).reset_index(drop=True)
-            df_BB_panel[col] = df_BB.apply(lambda row: pd.Series(row[col]), axis=1).stack().reset_index(level=1, drop=True)
+            # df_BB_panel[col] = df_BB.apply(lambda row: pd.Series(row[col]), axis=1).stack(dropna=False).reset_index(level=1, drop=True)
+            df_BB_panel[col] = df_BB.apply(lambda row: pd.Series(row[col]), axis=1).stack(dropna=False).values
             pass  # if
         pass  # for
     df_BB_panel = df_BB_panel.reset_index(drop=True)  # 重置索引
@@ -464,7 +464,8 @@ def fun_导入Pandas格式的实验结果数据转换为面板形式再导出(ex
     for col in list_columns_for_explode:  # 遍历其他需要展开的列，并将它们的元素展开以匹配 'id_agent' 列的行数
         if col != 'id_agent':
             # df_IB_panel[col] = df_IB[col].apply(lambda x: pd.Series(x.flatten())).stack().reset_index(level=0, drop=True).reset_index(drop=True)  # 对于二维数组需要展开两次
-            df_IB_panel[col] = df_IB[col].apply(lambda x: pd.Series(x.flatten())).stack().reset_index(level=1, drop=True)  # 对于二维数组需要展开两次
+            # df_IB_panel[col] = df_IB[col].apply(lambda x: pd.Series(x.flatten())).stack(dropna=False).reset_index(level=1, drop=True)  # 对于二维数组需要展开两次
+            df_IB_panel[col] = df_IB[col].apply(lambda x: pd.Series(x.flatten())).stack(dropna=False).values  # 对于二维数组需要展开两次
             pass  # if
         pass  # for
     df_IB_panel = df_IB_panel.reset_index(drop=True)  # 重置索引
