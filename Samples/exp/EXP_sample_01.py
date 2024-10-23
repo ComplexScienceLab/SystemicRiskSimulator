04ff95d2c7d812c5e8f9e56ec1401e428762c188
Lingo <ElementsPhoenix@163.com>
2024-10-23 23:18:30 +0800

    mod 更新模拟器示例代码

---
+++ Samples/exp/EXP_sample_01.py
@@ -0,0 +1,21 @@
+## 主程序示例
+
+from SystemicRiskSimulator.simulator import simulator
+
+
+def main():
+    # %% 配置项
+    config = dict()
+    config['folderpath_config'] = r"libraries/configs_library/config_sample_01"  # 配置项文件夹路径
+    config['foldername_simulator'] = r"SystemicRiskSimulator"  # 模拟器所在工程文件夹名称
+    config['folderpath_realpath_simulator'] = r"./Samples"  # 模拟器所在工程文件夹相对本实验项目根路径文件夹之相对路径
+    config['is_auto_confirmation'] = True  # 是否自动确认一些比较危险的操作例如删除、移动、复制文件等。默认 False；
+
+    # %% 运行模拟器
+    simulator(config)
+
+    pass  # function
+
+
+if __name__ == '__main__':
+    main()
