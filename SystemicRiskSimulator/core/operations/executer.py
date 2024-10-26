6bb1d890f2d791e02f7fd93a231355b46cb9377b
Lingo <ElementsPhoenix@163.com>
2024-10-26 22:12:23 +0800

    mod 简化框架

--- SystemicRiskSimulator/core/operations/executer.py
+++ SystemicRiskSimulator/core/operations/executer.py
@@ -4,7 +4,6 @@
 from SystemicRiskSimulator.external_packages import logging, pd, deepcopy, dataclass
 from SystemicRiskSimulator.core.define.define_agents import SystemicRiskAgent
 from SystemicRiskSimulator.core.define.define_agentDataCollection import AgentDataCollection
-from SystemicRiskSimulator.core.define.define_entity import Entity
 # from SystemicRiskSimulator.data.models.contents.content_finance import Finance
 from SystemicRiskSimulator.core.operations.collector import Collector
 
@@ -153,37 +152,5 @@ class Executer:
     #
     #     # return sgv
     #     pass  # function
-
-    @classmethod
-    def execute_main_model_entity(cls, A: SystemicRiskAgent, A_data: AgentDataCollection, para: dict, sgv: dict, entity: Entity):
-        """
-        执行过程版本（非流程版本）的模型实体（模型模板实体）。#HACK 似乎无用了。
-
-        NOTE：输入参数将被直接修改。
-
-        Args:
-            A (SystemicRiskAgent): Agent群变量
-            A_data (Optional[AgentDataCollection]): Agent群变量之数据
-            para (dict): 参数变量
-            sgv (dict): 模拟器全局变量
-            entity (Entity): 模型实体
-
-        Returns: agent
-
-        """
-        # entity = node.content  # 获取节点实体对应的模型实体
-
-        logging.debug("    开始执行模型内容：")
-
-        # sgv['turn'] += 1  # 计次轮次数（由于开始轮次是`START`，所以记为0）
-        # sgv['phase'] = 1  # 逐相复位（起始为1）
-        # # sgv['step'] += 1  # 步进加一
-        sgv['process_name'] = entity.attribute.entity_name  # 执行的过程之名称（英文名称）
-        entity.execute(A, A_data, para, sgv)
-
-        logging.debug("    结束执行模型内容。")
-
-        return A, A_data, sgv
-        pass  # function
 
     pass  # class
