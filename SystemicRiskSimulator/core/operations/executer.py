fa3b7f7a8bfcfcc88f7b79ad495a3417f3fb14ee
Lingo <ElementsPhoenix@163.com>
2024-11-24 00:02:04 +0800

    dev 自动更新变量需适配新框架

--- SystemicRiskSimulator/core/operations/executer.py
+++ SystemicRiskSimulator/core/operations/executer.py
@@ -112,7 +112,7 @@ class Executer:
 
         logging.debug(f"                步进：{sgv['step']}，相：{sgv['phase']}，更新源：{update_way}")
         # Finance.update_variables(A.BB, A.IB, A.b, A.ib, by_way=update_way)  # 更新金融变量
-        content.update_variables(A.BB, A.IB, A.b, A.ib, by_way=update_way)  # 更新金融变量
+        content.update_variables(A, by_way=update_way)  # 更新金融变量
         if is_collect or sgv['is_use_RLlib_frameworks'] is False or sgv['RL_state'] == 'using':
             Collector.collect_agent_data(A, A_data, sgv)
             pass  # if
