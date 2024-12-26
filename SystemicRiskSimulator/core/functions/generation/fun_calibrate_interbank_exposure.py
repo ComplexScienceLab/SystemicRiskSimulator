13aaefcfc13d914ec2e24b81519d3a26f85f62cd
Lingo <ElementsPhoenix@163.com>
2024-12-27 00:20:18 +0800

    mod fun_calibrate_interbank_exposure.py
    开发 calculate_bilateral_exposure_by_ME_algorithm_with_density ；

--- SystemicRiskSimulator/core/functions/generation/fun_calibrate_interbank_exposure.py
+++ SystemicRiskSimulator/core/functions/generation/fun_calibrate_interbank_exposure.py
@@ -731,7 +731,7 @@ def calculate_bilateral_exposure_by_ME_method_with_density(
             A_IB_2[k] = 0  # 去掉该连接
             # A_IB_2_sorted_idx = (np.delete(A_IB_2_sorted_idx[0], i), np.delete(A_IB_2_sorted_idx[1], i))  # 去掉索引和对应的值
             j += 1
-            if is_show_detal:  # 可视化双边敞口矩阵为热力图
+            if is_show_detal:  # 可视化双边敞口矩阵为热力图 #DEBUG 测试用
                 mask = np.where(A_IB_2 == 0, True, False)
                 A_IB_2_masked = np.ma.masked_where(mask, A_IB_2)
                 fig, ax = plt.subplots()
@@ -750,11 +750,12 @@ def calculate_bilateral_exposure_by_ME_method_with_density(
         pass  # while
 
     # 总结调整后的的连接密度
-    density = np.count_nonzero(A_IB_2) / num_edges_no_zeros
+    num_edges_adjasted = np.count_nonzero(A_IB_2)  # 计算非零元素的个数
+    density = num_edges_adjasted / num_edges_no_zeros
     # 完成率
     density / target_density
     logging.info(f"调整后的的连接密度：{density}")
-    if density < target_density:
+    if density > target_density:
         is_the_target_density = False
         logging.warning(f"调整后的的连接密度未达到目标密度：{target_density}")
     else:
@@ -1385,7 +1386,8 @@ if __name__ == "__main__":
 
     for i in range(0, 10):
         # 假设有 5 个银行
-        num_banks = 5
+        num_banks = 10
+        target_density = 0.75
 
         # 随机生成银行间总资产和总负债矩阵
         np.random.seed(i)  # 固定随机种子以便复现结果
@@ -1394,7 +1396,7 @@ if __name__ == "__main__":
         Z_IB_all = A_IB_all.sum() / Z_IB_all.sum() * Z_IB_all  # A_IB_all 与 Z_IB_all 之和相等
 
         # 调用函数计算双边敞口
-        A_IB_ij, Z_IB_ij = calculate_bilateral_exposure_by_ME_method_with_density(A_IB_all=A_IB_all, Z_IB_all=Z_IB_all, target_density=0.75, iteration_threshold=1e-3, is_show_detal=True, max_iteration=100, denominator_precition_threshold=1e-3)
+        A_IB_ij, Z_IB_ij, density, is_the_target_density = calculate_bilateral_exposure_by_ME_method_with_density(A_IB_all=A_IB_all, Z_IB_all=Z_IB_all, target_density=target_density, iteration_threshold=1e-10, is_show_detal=False, max_iteration=10000, denominator_precition_threshold=1e-10)
 
         # 打印结果
         print("银行间资产矩阵 A_IB_ij:")
@@ -1405,6 +1407,9 @@ if __name__ == "__main__":
         print(f"\n行元素和之误差占比：{np.abs(A_IB_ij.sum(axis=1) - A_IB_all) / A_IB_all}")
         print(f"\n列元素和之误差：{np.round(A_IB_ij.sum(axis=0) - Z_IB_all)}")
         print(f"\n列元素和之误差占比：{np.abs(A_IB_ij.sum(axis=0) - Z_IB_all) / Z_IB_all}")
+        print(f"\n实际密度：{density}")
+        print(f"\n目标密度：{target_density}")
+        print(f"\n是否达到目标密度：{is_the_target_density}")
 
         print(f"\n测试 calculate_bilateral_exposure_by_ME_method_with_density 第 {i + 1} 次 完成。\n\n\n")
 
