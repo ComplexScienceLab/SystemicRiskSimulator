1f62570eaeff8b92e2180d8e813cbb8a6a70bb42
Lingo <ElementsPhoenix@163.com>
2025-05-02 00:19:40 +0800

    重构 model_main 和 model_strategy 中的策略计算变量，以提高清晰度和正确性，适配 ABM 和 RL 相关的变量；

--- SystemicRiskSimulator/programs/experiments_program.py
+++ SystemicRiskSimulator/programs/experiments_program.py
@@ -364,8 +364,8 @@ def main(sgv):
                     #     actions.append(
                     #         M.model_algorithm[i].take_action(observations[i])  # 用RL算法选择个体动作
                     #     )
-                    agents_actions = M.convert_actions_to_env(actions)  # PyTorch 生成的动作转换为
-                    M.A.IB.theta_IB_def = agents_actions
+                    # agents_actions = M.convert_actions_to_env(M.A.AB.actions)  # PyTorch 生成的动作转换为
+                    # M.A.IB.theta_IB_def = M.A.AB.actions
                     M.model_action(A=M.A, A_data=M.A_data, para=M.para, sgv=M.sgv)
 
                     # 执行动作
