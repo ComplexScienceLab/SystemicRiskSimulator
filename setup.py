6206b156270e78655f7781b21bee374cd6920aa7
Lingo <ElementsPhoenix@163.com>
2025-05-11 16:19:10 +0800

    manage 实现 pip 打包
    添加 MANIFEST.in、setup.py 和更新 pyproject.toml，配置项目打包信息

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="systemic_risk_simulator",
+    version="0.0.46_alpha",
+    description="一个金融系统的系统性风险模拟器。 A systemic risk simulator for financial systems.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Ethan Lin",
+    author_email="ElementsSystem@hunnu.edu.cn",
+    url="https://gitee.com/EthanLingo/SystemicRiskSimulator",
+    packages=find_packages(where="SystemicRiskSimulator"),
+    package_dir={"": "SystemicRiskSimulator"},
+    include_package_data=True,
+    python_requires=">=3.11",
+    install_requires=[
+        "chardet==5.2.0",
+        "drawsvg==2.4.0",
+        "gymnasium>=1.1.1",
+        "matplotlib==3.10.0",
+        "networkx==3.4.2",
+        "numpy==2.2.1",
+        "openpyxl==3.1.5",
+        "pandas==2.2.3",
+        "pillow==11.1.0",
+        "pip>=25.1",
+        "pymupdf==1.25.3",
+        "reportlab==4.2.5",
+        "rpy2==3.5.17",
+        "scipy==1.15.1",
+        "screeninfo==0.8.1",
+        "setuptools>=80.0.1",
+    ],
+    license="GNU AGPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3.11",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": [
+            "systemicrisksimulator=SystemicRiskSimulator.simulator:simulator",
+        ],
+    },
+)
