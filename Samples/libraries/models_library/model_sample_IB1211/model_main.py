3ced2e8d7945d59217fae51c227afea8e6ec79ee
Lingo <ElementsPhoenix@163.com>
2025-05-24 21:05:39 +0800

    示例模型：更新 model_main.py，添加 is_collect 和 collect 参数以支持数据收集。
    但是注意还是没有完全适配；

--- Samples/libraries/models_library/model_sample_IB1211/model_main.py
+++ Samples/libraries/models_library/model_sample_IB1211/model_main.py
@@ -38,7 +38,7 @@ class ModelMain:
         """
 
         ## node_START
-        Executer.update_variableStep(self.model_finance, 'all', A, A_data, para, sgv)  # 更新各银行之所有变量，在第一回合开始时
+        Executer.update_variableStep(self.model_finance, 'all', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 更新各银行之所有变量，在第一回合开始时
 
         ## node_010 外部资产违约损失冲击
         isv_last = A.BB.isv.copy()
@@ -56,14 +56,14 @@ class ModelMain:
         A.DA.p[A.DA.inf] = (A.DA.DA[A.DA.inf] - A.DA.Shock_DA_def_t[A.DA.inf]) / A.DA.DA[A.DA.inf]  # 贷款资产单位价格变动
 
         A.IBA.Shock_IBA_def[:, A.DA.inf] = A.IBA.IBA[:, A.DA.inf] * (1 - A.DA.p[A.DA.inf])  # 计算银行持有共同贷款类资产之违约损失冲击
-        Executer.update_variableStep(self.model_finance, 'Shock_IBA_def', A, A_data, para, sgv)
+        Executer.update_variableStep(self.model_finance, 'Shock_IBA_def', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])
 
         # 计算银行持有共同资产之损失量
         A.IBA.Loss_IBA += A.IBA.Shock_IBA_def
         A.BB.Loss_IBA_def_t += A.BB.Shock_IBA_def_t
 
         A.BB.Shock_P_def_t[A.BB.exist] = A.BB.Shock_IBA_def_t[A.BB.exist]  # 银行持有贷款类资产之违约损失冲击
-        Executer.update_variableStep(self.model_finance, 'Shock_P_def_t', A, A_data, para, sgv)  # 厂商贷款违约损失冲击传导至银行内资产冲击
+        Executer.update_variableStep(self.model_finance, 'Shock_P_def_t', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 厂商贷款违约损失冲击传导至银行内资产冲击
 
         A.BB.inf = ~A.BB.br & (A.BB.Shock_P_def_t > 0.0)
         logging.debug(f"          轮次 {sgv['turn']}：模型 {sgv['process_name']}，银行遭受感染情况：{A.BB.inf}")
@@ -72,16 +72,16 @@ class ModelMain:
         # A.BB.Z_other[~A.BB.br] += A.BB.Z_other[~A.BB.br] + A.BB.Z_all[A.BB.br]  # 破产清算
 
         A.BB.Loss_exIB_def_t[A.BB.inf] = A.BB.Shock_P_def_t[A.BB.inf]  # 非银行间违约损失冲击损失
-        Executer.update_variableStep(self.model_finance, 'Loss_exIB_def_t', A, A_data, para, sgv)
+        Executer.update_variableStep(self.model_finance, 'Loss_exIB_def_t', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])
 
         A.BB.Default_IB_def_s[A.BB.inf] += np.minimum(np.maximum(A.BB.Loss_exIB_def_t[A.BB.inf] + A.BB.Loss_IB_def_t[A.BB.inf] - A.BB.E_all[A.BB.inf], 0), A.BB.Z_IB_all[A.BB.inf])  # 银行间违约量变动
-        Executer.update_variableStep(self.model_finance, 'Default_IB_def_s', A, A_data, para, sgv)  # 更新银行间违约量
+        Executer.update_variableStep(self.model_finance, 'Default_IB_def_s', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 更新银行间违约量
 
         A.BB.Default_D_def_s[A.BB.inf] = np.maximum(A.BB.Loss_exIB_def_t[A.BB.inf] + A.BB.Loss_IB_def_t[A.BB.inf] - (A.BB.E_all[A.BB.inf] + A.BB.Z_IB_all[A.BB.inf]), 0)  # 商业银行对居民存款应违约量变动
-        Executer.update_variableStep(self.model_finance, 'Default_D_def_s', A, A_data, para, sgv)  # 更新商业银行对居民存款应违约量
+        Executer.update_variableStep(self.model_finance, 'Default_D_def_s', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 更新商业银行对居民存款应违约量
 
         A.BB.Shock_IB_def_s[A.BB.inf] = A.BB.Default_IB_def_s[A.BB.inf]  # 计算应银行内冲击传导至银行间传染冲击
-        Executer.update_variableStep(self.model_finance, 'Shock_IB_def_s', A, A_data, para, sgv)  # 更新违约损失冲击源头变量Shock_def_s
+        Executer.update_variableStep(self.model_finance, 'Shock_IB_def_s', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 更新违约损失冲击源头变量Shock_def_s
 
         ## node_015 外部资产违约损失冲击
         IDA_exp_factor = np.zeros((sgv['num_bank'], sgv['num_asset']))  # 银行持有贷款类资产之指数传染因子
@@ -91,8 +91,8 @@ class ModelMain:
         logging.debug(f"          轮次 {sgv['turn']}：模型 {sgv['process_name']}，银行传染出去情况：{A.BB.con}")
         A.BB.br |= A.BB.isv  # 更新破产银行
 
-        Executer.update_variableStep(self.model_finance, 'clear all Shock_target', A, A_data, para, sgv)  # 清零所有冲击目标变量 Shock_target
-        Executer.update_variableStep(self.model_finance, 'clear all Shock_interbank', A, A_data, para, sgv)  # 清零所有银行间冲击变量 Shock_IB
+        Executer.update_variableStep(self.model_finance, 'clear all Shock_target', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 清零所有冲击目标变量 Shock_target
+        Executer.update_variableStep(self.model_finance, 'clear all Shock_interbank', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 清零所有银行间冲击变量 Shock_IB
 
         while (A.BB.inf.any() and sgv['is_continue_process']):
 
@@ -113,9 +113,9 @@ class ModelMain:
                 0
             )
             # A.IB.Shock_IB_def[A.BB.isv, :] = np.where(np.abs(A.IB.Shock_IB_def[A.BB.isv, :]) > 1.0e0, A.IB.Shock_IB_def[A.BB.isv, :], 0.0)  # 如果出现冲击值很小，小于一个阈值的时候，那么就设置为0
-            Executer.update_variableStep(self.model_finance, 'Shock_IB_def', A, A_data, para, sgv)
+            Executer.update_variableStep(self.model_finance, 'Shock_IB_def', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])
 
-            Executer.update_variableStep(self.model_finance, 'clear all Shock_source', A, A_data, para, sgv)  # 清零所有冲击源头变量 Shock_source
+            Executer.update_variableStep(self.model_finance, 'clear all Shock_source', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 清零所有冲击源头变量 Shock_source
 
             A.BB.inf = ~A.BB.br & (A.BB.Shock_IB_def_t > 0.0)
             logging.debug(f"          轮次 {sgv['turn']}：模型 {sgv['process_name']}，银行遭受传染情况：{A.BB.inf}")
@@ -135,35 +135,35 @@ class ModelMain:
             A.IB.Default_IB += A.IB.Shock_IB_def  # 更新 Default_IB
 
             A.IB.Loss_IB_def += A.IB.Shock_IB_def.T  # 计算 Loss_IB_def
-            Executer.update_variableStep(self.model_finance, 'Loss_IB_def_t', A, A_data, para, sgv)
+            Executer.update_variableStep(self.model_finance, 'Loss_IB_def_t', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])
 
             # 资不抵债银行资产违约损失冲击模型 InterBankInsolventShock
 
             A.BB.Loss_IB_def_t += A.BB.Shock_IB_def_t  # 银行间违约损失冲击损失
-            Executer.update_variableStep(self.model_finance, 'Loss_IB_def_t', A, A_data, para, sgv)
+            Executer.update_variableStep(self.model_finance, 'Loss_IB_def_t', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])
 
             # 临时记录 A.BB.Default_IB_def_s 、A.BB.Default_D_def_s 变动前的值
             Default_IB_def_s_last = A.BB.Default_IB_def_s.copy()
             Default_D_def_s_last = A.BB.Default_D_def_s.copy()
 
             A.BB.Default_IB_def_s[A.BB.inf] = np.minimum(np.maximum(A.BB.Loss_exIB_def_t[A.BB.inf] + A.BB.Loss_IB_def_t[A.BB.inf] - A.BB.E_all[A.BB.inf], 0), A.BB.Z_IB_all[A.BB.inf])  # 银行间违约量变动
-            Executer.update_variableStep(self.model_finance, 'Default_IB_def_s', A, A_data, para, sgv)  # 更新银行间违约量
+            Executer.update_variableStep(self.model_finance, 'Default_IB_def_s', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 更新银行间违约量
 
             A.BB.Default_D_def_s[A.BB.inf] = np.maximum(A.BB.Loss_exIB_def_t[A.BB.inf] + A.BB.Loss_IB_def_t[A.BB.inf] - (A.BB.E_all[A.BB.inf] + A.BB.Z_IB_all[A.BB.inf]), 0)  # 商业银行对居民存款应违约量变动
-            Executer.update_variableStep(self.model_finance, 'Default_D_def_s', A, A_data, para, sgv)
+            Executer.update_variableStep(self.model_finance, 'Default_D_def_s', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])
 
             A.BB.Shock_IB_def_s[A.BB.inf] = A.BB.Default_IB_def_s[A.BB.inf] - Default_IB_def_s_last[A.BB.inf]  # 计算银行内冲击传导至银行间传染冲击
-            Executer.update_variableStep(self.model_finance, 'Shock_IB_def_s', A, A_data, para, sgv)
+            Executer.update_variableStep(self.model_finance, 'Shock_IB_def_s', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])
 
             A.BB.Shock_D_def_s[A.BB.inf] = A.BB.Default_D_def_s[A.BB.inf] - Default_D_def_s_last[A.BB.inf]  # 计算应银行内冲击传导至居民存款传染冲击
-            Executer.update_variableStep(self.model_finance, 'Shock_D_def_s', A, A_data, para, sgv)
+            Executer.update_variableStep(self.model_finance, 'Shock_D_def_s', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])
 
             A.BB.con = ~A.BB.br & (A.BB.Shock_IB_def_s > 0.0)
             logging.debug(f"          轮次 {sgv['turn']}：模型 {sgv['process_name']}，银行传染出去情况：{A.BB.con}")
             A.BB.br |= A.BB.isv  # 更新破产银行
 
-            Executer.update_variableStep(self.model_finance, 'clear all Shock_target', A, A_data, para, sgv)  # 清零所有冲击目标变量 Shock_target
-            Executer.update_variableStep(self.model_finance, 'clear all Shock_interbank', A, A_data, para, sgv)  # 清零所有银行间冲击变量 Shock_IB
+            Executer.update_variableStep(self.model_finance, 'clear all Shock_target', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 清零所有冲击目标变量 Shock_target
+            Executer.update_variableStep(self.model_finance, 'clear all Shock_interbank', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 清零所有银行间冲击变量 Shock_IB
 
             ## node_030 持有共同贷款资产价格损失冲击
 
@@ -174,7 +174,7 @@ class ModelMain:
 
             # 计算银行持有贷款类资产之指数传染因子，从而得到银行持有贷款类资产之单价变动
 
-            Executer.update_variableStep(self.model_finance, 'Shock_IBA_def', A, A_data, para, sgv)  # 计算银行持有共同贷款资产违约损失冲击目标
+            Executer.update_variableStep(self.model_finance, 'Shock_IBA_def', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 计算银行持有共同贷款资产违约损失冲击目标
 
             A.IBA.IDA_exp_factor = np.zeros((sgv['num_bank'], sgv['num_asset']))  # 银行持有贷款类资产单价变动后的值
             A.IBA.IDA_exp_factor[np.ix_(A.BB.inf, A.DA.inf)] = np.exp(-para['kappa_BA'] * (A.IBA.IBA[np.ix_(A.BB.inf, A.DA.inf)] / A.DA.DA[A.DA.inf]))  # 计算银行持有贷款类资产之指数传染因子
@@ -182,7 +182,7 @@ class ModelMain:
             # 计算银行受到持有共同贷款资产价格损失冲击
             A.BB.Shock_IBA_def_t = np.zeros(sgv['num_bank'])
             A.IBA.Shock_IBA_def[:, A.DA.inf] = A.IBA.IBA[:, A.DA.inf] * (1 - A.IBA.IDA_exp_factor[np.ix_(A.BB.inf, A.DA.inf)]).sum(axis=0)  # 计算银行持有贷款类资产之冲击
-            Executer.update_variableStep(self.model_finance, 'Shock_IBA_def', A, A_data, para, sgv)  # 更新银行持有共同贷款资产违约损失冲击
+            Executer.update_variableStep(self.model_finance, 'Shock_IBA_def', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 更新银行持有共同贷款资产违约损失冲击
 
             # 计算银行持有共同资产之损失量
             A.IBA.Loss_IBA += A.IBA.Shock_IBA_def
@@ -190,7 +190,7 @@ class ModelMain:
 
             A.IBA.IBA[:, A.DA.inf] = A.IBA.IBA[:, A.DA.inf] * np.prod(A.IBA.IDA_exp_factor[np.ix_(A.BB.inf, A.DA.inf)], axis=0)  # 计算银行持有贷款类资产
 
-            Executer.update_variableStep(self.model_finance, 'IBA', A, A_data, para, sgv)  # 更新银行持有共同贷款资产
+            Executer.update_variableStep(self.model_finance, 'IBA', A, A_data, para, sgv, is_collect=True, collect=sgv['collect'])  # 更新银行持有共同贷款资产
 
             # 更新示性向量之于银行当前轮次是否传染出去
             A.BB.con = ~A.BB.br & (A.BB.Shock_t > 0.0)
